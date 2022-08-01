2b3ac16c07e19359f81ab23661eee3bd3766900b
mihaw <779589950@qq.com>
2022-08-01 15:17:27 +0800

    add download test

---
+++ tests/ut/utils/test_download.py
@@ -0,0 +1,59 @@
+# Copyright 2022 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""
+Test Download
+"""
+
+import unittest
+import os
+from text.utils.download import get_cache_path, check_md5, get_dataset_url
+
+class TestGetCachePath(unittest.TestCase):
+    r"""
+    Test get_cache_path
+    """
+
+    def setUp(self):
+        self.input = None
+
+    def test_get_cache_path(self):
+        default_cache_path = get_cache_path()
+        assert default_cache_path == (os.path.expanduser('~')+"/.text")
+
+class TestCheckMd5(unittest.TestCase):
+    r"""
+    Test check_md5
+    """
+
+    def setUp(self):
+        self.input = None
+
+    def test_check_md5(self):
+        filename = "test"
+        check_md5_result = check_md5(filename)
+        assert check_md5_result
+
+class TestGetDatasetUrl(unittest.TestCase):
+    r"""
+    Test get_dataset_url
+    """
+
+    def setUp(self):
+        self.input = None
+
+    def test_get_dataset_url(self):
+        name = 'aclImdb_v1'
+        url = get_dataset_url(name)
+        assert url == 'https://mindspore-website.obs.myhuaweicloud.com/notebook/datasets/aclImdb_v1.tar.gz'
