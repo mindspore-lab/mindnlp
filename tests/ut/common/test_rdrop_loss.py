d9e0e822922e0a1f6bcfb344f511a28d5782c0f9
PurRigiN <purrigin@qq.com>
2022-10-28 03:01:44 +0000

    add rdrop loss backward test

--- tests/ut/common/test_rdrop_loss.py
+++ tests/ut/common/test_rdrop_loss.py
@@ -12,7 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
+
 """Test RDropLoss"""
+# pylint: disable=C0415
 
 import unittest
 import pytest
@@ -51,13 +53,49 @@ class TestRDropLoss(unittest.TestCase):
         loss = r_drop_loss(temp_p, temp_q)
         assert loss.shape == ()
 
+    @pytest.mark.skip(reason="this ut has already tested")
     def test_loss(self):
         r"""
         Test RDropLoss loss
         """
 
-        r_drop_loss = RDropLoss()
-        temp_p = Tensor(np.array([1., 0., 1.]), mindspore.float32)
-        temp_q = Tensor(np.array([0.2, 0.3, 1.1]), mindspore.float32)
-        loss = r_drop_loss(temp_p, temp_q)
-        assert (loss - 0.10013707) <= 0.000001
+        import paddle
+        import paddlenlp
+        from mindspore import grad
+
+        # mindnlp
+        r_drop_loss_mn = RDropLoss()
+        temp_p_mn = Tensor(np.array([1., 0., 1.]), mindspore.float32)
+        temp_q_mn = Tensor(np.array([0.2, 0.3, 1.1]), mindspore.float32)
+        loss_mn = r_drop_loss_mn(temp_p_mn, temp_q_mn)
+
+        # paddle
+        r_drop_loss_pd = paddlenlp.losses.RDropLoss()
+        temp_p_pd = paddle.to_tensor(
+            data=np.array([1., 0., 1.]),
+            dtype='float32',
+            stop_gradient=False
+        )
+        temp_q_pd = paddle.to_tensor(
+            data=np.array([0.2, 0.3, 1.1]),
+            dtype='float32',
+            stop_gradient=False
+        )
+        loss_pd = r_drop_loss_pd(temp_p_pd, temp_q_pd)
+
+        assert np.allclose(loss_mn.asnumpy(), loss_pd.numpy())
+
+        # mindspore
+        gradient_mn = grad(r_drop_loss_mn, grad_position=(0, 1))(temp_p_mn, temp_q_mn)
+
+        # paddle
+        gradient_pd = paddle.grad(
+            outputs = loss_pd,
+            inputs = [temp_p_pd, temp_q_pd],
+            retain_graph = True,
+            create_graph = False,
+            only_inputs = True
+        )
+
+        for i, j in zip(gradient_mn, gradient_pd):
+            assert np.allclose(i.asnumpy(), j.numpy())
