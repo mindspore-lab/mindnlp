edc24556b515584bc98a26ba40f7081e550cde3d
lvyufeng <lvyufeng@cqu.edu.cn>
2023-02-06 15:45:07 +0800

    use while loop to speed up CRF

--- tests/ut/modules/test_crf.py
+++ tests/ut/modules/test_crf.py
@@ -18,13 +18,17 @@ Test CRF
 # pylint: disable=W0640
 
 import itertools
+import unittest
 import math
 import random
 import pytest
 import mindspore
 import numpy as np
+from ddt import ddt, data
 from mindspore import ops
+from mindnlp import ms_jit
 from mindnlp.modules import CRF
+from mindnlp.modules.crf import sequence_mask
 
 RANDOM_SEED = 0
 
@@ -119,13 +123,14 @@ class TestInit:
             CRF(0)
         assert 'invalid number of tags: 0' in str(excinfo.value)
 
-
-class TestForward:
+@ddt
+class TestForward(unittest.TestCase):
     """
     Test forward
     """
 
-    def test_works_without_mask(self):
+    @data(True, False)
+    def test_works_without_mask(self, jit):
         """
         Test works without mask
         """
@@ -135,9 +140,16 @@ class TestForward:
         # shape: (seq_length, batch_size)
         tags = make_tags(crf)
 
-        llh_no_mask = crf(emissions, tags)
-        # No mask means the mask is all ones
-        llh_mask = crf(emissions, tags, seq_length=ops.fill(mindspore.int64, (tags.shape[1],), tags.shape[0]))
+        def forward(emissions, tags):
+            llh_no_mask = crf(emissions, tags)
+            # No mask means the mask is all ones
+            llh_mask = crf(emissions, tags, seq_length=ops.fill(mindspore.int64, (tags.shape[1],), tags.shape[0]))
+            return llh_no_mask, llh_mask
+
+        if jit:
+            forward = ms_jit(forward)
+        
+        llh_no_mask, llh_mask = forward(emissions, tags)
 
         assert np.allclose(llh_no_mask.asnumpy(), llh_mask.asnumpy(),rtol=1e-3,atol=1e-3)
 
@@ -264,3 +276,108 @@ class TestForward:
         llh_bf = crf_bf(emissions_bf, tags_bf)
 
         assert np.allclose(llh.asnumpy(), llh_bf.asnumpy(), rtol=1e-3, atol=1e-3)
+
+@ddt
+class TestDecode(unittest.TestCase):
+
+    @data(True, False)
+    def test_works_with_mask(self, jit):
+        crf = make_crf()
+        seq_length, batch_size = 3, 2
+        # shape: (seq_length, batch_size, num_tags)
+        emissions = make_emissions(crf, seq_length, batch_size)
+        # seq_length should be (batch_size,)
+        seq_length = mindspore.Tensor([3, 2], mindspore.int64)
+
+        def forward(emissions, seq_length):
+            score, history = crf(emissions, seq_length=seq_length)
+            return score, history
+
+        if jit:
+            forward = ms_jit(forward)
+        
+        score, history = forward(emissions, seq_length)
+        best_tags = crf.post_decode(score, history, seq_length)
+        # shape: (batch_size, seq_length, num_tags)
+        emissions = emissions.swapaxes(0, 1)
+        # shape: (batch_size, seq_length)
+        mask = sequence_mask(seq_length, emissions.shape[1], True)
+
+        # Compute best tag manually
+        for emission, best_tag, mask_ in zip(emissions, best_tags, mask):
+            seq_len = mask_.sum()
+            assert len(best_tag) == seq_len
+            # assert all(isinstance(t, int) for t in best_tag)
+            emission = emission[:seq_len]
+            manual_best_tag = max(
+                itertools.product(range(crf.num_tags), repeat=seq_len),
+                key=lambda t: compute_score(crf, emission, t))
+
+            print(best_tag)
+            assert tuple(best_tag) == manual_best_tag
+
+    def test_works_without_mask(self):
+        crf = make_crf()
+        # shape: (seq_length, batch_size, num_tags)
+        emissions = make_emissions(crf)
+
+        best_tags_no_mask = crf(emissions)
+        # No mask means mask is all ones
+        best_tags_mask = crf._viterbi_decode(
+            emissions, mask= ops.ones(emissions.shape[:2], mindspore.bool_))
+
+        assert (best_tags_no_mask[0] == best_tags_mask[0]).all()
+
+    def test_batched_decode(self):
+        crf = make_crf()
+        batch_size, seq_length = 2, 3
+
+        # shape: (seq_length, batch_size, num_tags)
+        emissions = make_emissions(crf, seq_length, batch_size)
+        # shape: (seq_length, batch_size)
+        batched = crf(emissions)[0]
+
+        non_batched = []
+        for i in range(batch_size):
+            # shape: (seq_length, 1, num_tags)
+            emissions_ = emissions[:, i, :].unsqueeze(1)
+            # shape: (seq_length, 1)
+            result = crf(emissions_)
+            assert len(result) == 2
+            non_batched.append(result[0])
+
+        non_batched = ops.concat(non_batched)
+        assert (non_batched == batched).all()
+
+    def test_batch_first(self):
+        crf = make_crf()
+        # shape: (seq_length, batch_size, num_tags)
+        emissions = make_emissions(crf)
+        best_tags = crf(emissions)
+
+        crf_bf = make_crf(batch_first=True)
+        # Copy parameter values from non-batch-first CRF; requires_grad must be False
+        # to avoid runtime error of in-place operation on a leaf variable
+        crf_bf.start_transitions.set_data(crf.start_transitions)
+        crf_bf.end_transitions.set_data(crf.end_transitions)
+        crf_bf.transitions.set_data(crf.transitions)
+
+        # shape: (batch_size, seq_length, num_tags)
+        emissions = emissions.swapaxes(0, 1)
+        best_tags_bf = crf_bf(emissions)
+
+        assert (best_tags[0] == best_tags_bf[0]).all()
+
+    def test_emissions_has_bad_number_of_dimension(self):
+        emissions = ops.randn(1, 2)
+        crf = make_crf()
+
+        with pytest.raises(ValueError) as excinfo:
+            crf(emissions)
+
+    def test_emissions_last_dimension_not_equal_to_number_of_tags(self):
+        emissions = ops.randn(1, 2, 3)
+        crf = make_crf(10)
+
+        with pytest.raises(ValueError) as excinfo:
+            crf(emissions)
