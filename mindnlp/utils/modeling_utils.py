93f5c7970fca625249215625d36b28a11c058afd
Your Name <you@example.com>
2023-03-11 22:31:11 +0800

    add utils_modeling_util.

---
+++ mindnlp/utils/modeling_utils.py
@@ -0,0 +1,96 @@
+# Copyright 2022 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""MindNLP Model Utils"""
+
+from typing import Optional, Tuple, Union, Callable
+
+import mindspore
+
+from activations import get_activation, ACT2CLS
+from mindspore import nn, ops, Parameter, Tensor, dtype_to_nptype
+
+from .configuration_utils import PretrainedConfig
+
+
+try:
+    from mindspore.nn import Identity
+except ImportError:
+    # Older MindSpore compatibility
+    class Identity(nn.cell):
+        r"""A placeholder identity operator that is argument-insensitive."""
+
+        def __init__(self, *args, **kwargs):
+            super().__init__()
+
+        def construct(self, hidden_states):
+            return hidden_states
+
+
+# class SequenceSummary(nn.Cell):
+#     """
+#     GPT2DoubleHeadsModel class that self.multiple_choice_head
+#     """
+#     def __init__(self, config):
+#         super().__init__()
+# 
+#         self.summary_type = getattr(config, "summary_type", "last")
+#         if self.summary_type == "attn":
+#             raise NotImplementedError
+# 
+#         self.summary = Identity()
+#         if hasattr(config, "summary_use_proj") and config.summary_use_proj:
+#             if hasattr(config, "summary_proj_to_labels") and config.summary_proj_to_labels and config.num_labels > 0:
+#                 num_classes = config.num_labels
+#             else:
+#                 num_classes = config.hidden_size
+#             self.summary = nn.Dense(config.hidden_size, num_classes)
+# 
+#         activation_string = getattr(config, "summary_activation", None)
+#         self.activation = get_activation(activation_string) if activation_string else Identity()
+# 
+#         self.first_dropout = Identity()
+#         if hasattr(config, "summary_first_dropout") and config.summary_first_dropout > 0:
+#             self.first_dropout = nn.Dropout(p=config.summary_first_dropout)
+# 
+#         self.last_dropout = Identity()
+#         if hasattr(config, "summary_last_dropout") and config.summary_last_dropout > 0:
+#             self.last_dropout = nn.Dropout(p=config.summary_last_dropout)
+# 
+#     def construct(self, hidden_states: Tensor, cls_index: Optional[Tensor] = None) -> Tensor:
+#         if self.summary_type == "last":
+#             output = hidden_states[:, -1, :]
+#         elif self.summary_type == "first":
+#             output = hidden_states[:, 0, :]
+#         elif self.summary_type == "mean":
+#             output = hidden_states.mean(dim=1)
+#         elif self.summary_type == "cls_index":
+#             if cls_index is None:
+#                 cls_index = ops.full_like(
+#                     hidden_states[..., :1, :],
+#                     hidden_states.shape[-2] - 1,
+#                     dtype=mindspore.int64,
+#                 )
+#             else:
+#                 cls_index = cls_index.unsqueeze(-1).unsqueeze(-1)
+#                 cls_index = cls_index.expand((-1,) * (cls_index.ndim - 1) + (hidden_states.shape[-1],))
+#             output = hidden_states.gather_elements(-2, cls_index).squeeze(-2)  # shape (bsz, XX, hidden_size)
+#         elif self.summary_type == "attn":
+#             raise NotImplementedError
+# 
+#         output = self.first_dropout(output)
+#         output = self.summary(output)
+#         output = self.activation(output)
+#         output = self.last_dropout(output)
+#         return output
