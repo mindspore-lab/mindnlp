d73f506a772227ef0f7c92988c8db4130bf63055
kuoyee <838154900@qq.com>
2023-03-13 21:24:05 +0800

    xlmpredlayer

--- mindnlp/models/xlm/xlm_config.py
+++ mindnlp/models/xlm/xlm_config.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# pylint: disable=too-many-instance-attributes
 """ XLM configuration"""
 from ...abc.backbones import PretrainedConfig
 from ...utils import logging
