497fc819da062d2a5978fb59d2b1d3a4c5d62aa2
lvyufeng <lvyufeng@cqu.edu.cn>
2023-03-21 20:02:07 +0800

    use pythonic vocab and support minddata pipeline

--- mindnlp/vocab/vocab.py
+++ mindnlp/vocab/vocab.py
@@ -21,61 +21,49 @@ import os
 import re
 import warnings
 from typing import Union
-from collections import OrderedDict
-import mindspore._c_dataengine as cde
+from mindspore.dataset import TextBaseDataset
 from mindnlp.configs import DEFAULT_ROOT
 from mindnlp.utils import cache_file
-
 
 class Vocab:
     r"""
     Creates a vocab object which maps tokens to indices.
     """
 
-    def __init__(self, list_or_dict: Union[list, OrderedDict, None] = None,
-                 special_tokens: Union[list, tuple] = ("<pad>", "<unk>"),
+    def __init__(self, list_or_dict: Union[list, dict],
+                 special_tokens: Union[list, tuple] = None,
                  special_first: bool = True):
 
-        self._dict = None
-        self._unk_token = None
-        self._c_vocab = None
+        self._token_dict = {}
 
-        if list_or_dict is None:
-            return
-
-        self._set_unk(special_tokens)
+        sp_len = len(special_tokens) if special_tokens is not None and special_first else 0
 
         if isinstance(list_or_dict, list):
-            self._dict = OrderedDict.fromkeys(list_or_dict)
-        elif isinstance(list_or_dict, OrderedDict):
-            self._dict = list_or_dict
+            for index, value in enumerate(list_or_dict):
+                self._token_dict[value] = index + sp_len
+        elif isinstance(list_or_dict, dict):
+            for key, value in list_or_dict.items():
+                if not isinstance(key, str):
+                    raise ValueError(f'keys in dict must be str, but got {type(key)}')
+                if not isinstance(value, int):
+                    raise ValueError(f'values in dict must be int, but got {type(key)}')
+                self._token_dict[key] = value + sp_len
         else:
-            raise ValueError(f'Vocab only support list or OrderedDict, but get {type(list_or_dict)}')
+            raise ValueError(f'Vocab only support list or dict, but get {type(list_or_dict)}')
 
-        if special_first:
-            self._dict.update((index, value) for index, value in enumerate(special_tokens))
-        else:
-            dict_len = len(self._dict)
+        if special_tokens is not None:
+            offset = 0 if special_first else len(self._token_dict)
             for idx, tok in enumerate(special_tokens):
-                self._dict[tok] = dict_len + idx
+                self._token_dict[tok] = idx + offset
 
-        self._c_vocab = cde.Vocab.from_dict(self._dict)
-
-    def _set_unk(self, special_tokens):
-        if '<unk>' in special_tokens:
-            self._unk_token = '<unk>'
-        else:
-            warnings.warn("Warning: can not find '<unk>' in vocab, "
-                          "please use Vocab.set_default_index() to set index of unknown token.",
-                          UserWarning)
-
+        self._index_dict = {v: k for k, v in self._token_dict.items()}
 
     def __len__(self) -> int:
         r"""
         Returns:
             - int, The length of the vocab.
         """
-        return len(self._dict)
+        return len(self._token_dict)
 
     def __contains__(self, token: str) -> bool:
         r"""
@@ -85,7 +73,7 @@ class Vocab:
         Returns:
             - bool, Whether the token is member of vocab or not.
         """
-        return token in self._dict
+        return token in self._token_dict
 
     def __getitem__(self, token: str) -> int:
         r"""
@@ -96,7 +84,99 @@ class Vocab:
             - int, The index corresponding to the associated token.
         """
 
-        return self._dict.get(token, None)
+        return self._token_dict.get(token, None)
+
+
+    def __call__(self, token_or_id):
+        if isinstance(token_or_id, str):
+            return self._token_dict.get(token_or_id, None)
+        if isinstance(token_or_id, int):
+            return self._index_dict.get(token_or_id, None)
+
+        raise ValueError(f'not support token type {type(token_or_id)}')
+
+    def lookup_ids(self, token_or_list):
+        """
+        Converts a token string or a sequence of tokens in a single integer id or a sequence of ids.
+
+        Args:
+            token_or_list (Union[str, list[str]]): One or several token(s) to convert to token id(s).
+
+        Returns:
+            - list[int], The token id or list of token ids.
+              if only one token used to lookup,
+              return one id instead of a list of ids.
+
+        Examples:
+            >>> import mindspore.dataset.text as text
+            >>> vocab = text.Vocab.from_list(["w1", "w2", "w3"], special_tokens=["<unk>"], special_first=True)
+            >>> ids = vocab.lookup_ids(["w1", "w3"])
+        """
+        if isinstance(token_or_list, str):
+            return self._token_dict.get(token_or_list)
+
+        if isinstance(token_or_list, list):
+            return_list = []
+            for token in token_or_list:
+                if token not in self._token_dict:
+                    raise ValueError(f"{token} is not in vocab.")
+                return_list.append(self._token_dict[token])
+            return return_list
+
+        raise ValueError(f'lookup only support str and list, but got {type(token_or_list)}.')
+
+    def lookup_tokens(self, index_or_list):
+        """
+        Converts a single index or a sequence of indices in a token or a sequence of tokens.
+        If id does not exist, return empty string.
+
+        Args:
+            index_or_list (Union[int, list[int]]): The token id (or token ids) to convert to tokens.
+
+        Returns:
+            - List<str>, The decoded token(s).
+              if only one id used to lookup,
+              return one token instead of a list of tokens.
+
+        Raises:
+            RuntimeError: If 'ids' is not in vocab.
+
+        Examples:
+            >>> import mindspore.dataset.text as text
+            >>> vocab = text.Vocab.from_list(["w1", "w2", "w3"], special_tokens=["<unk>"], special_first=True)
+            >>> token = vocab.lookup_tokens(0)
+        """
+        if isinstance(index_or_list, int):
+            return self._index_dict.get(index_or_list, None)
+
+        if isinstance(index_or_list, list):
+            return_list = []
+            for idx in index_or_list:
+                if idx not in self._index_dict:
+                    raise ValueError(f"{idx} is not in vocab.")
+                return_list.append(self._index_dict[idx])
+            return return_list
+
+        raise ValueError(f'lookup only support int and list, but got {type(index_or_list)}.')
+
+    def append_token(self, token):
+        r"""
+        Args:
+            token (str): The token used to lookup the corresponding index.
+
+        Raises:
+            RuntimeError: If `token` already exists in the vocab.
+
+        """
+        if isinstance(token, str):
+            if token in self._token_dict:
+                warnings.warn(f"{token} already exists in the vocab.")
+            else:
+                append_id = len(self._token_dict)
+                self._token_dict[token] = append_id
+                self._index_dict[append_id] = token
+        else:
+            raise TypeError(f"{token} is not str.")
 
     @classmethod
     def from_dataset(cls, dataset, columns=None, freq_range=None, top_k=None, special_tokens=None, special_first=True):
@@ -139,115 +219,14 @@ class Vocab:
             >>> dataset = dataset.map(operations=text.Lookup(vocab, "<unk>"), input_columns=["text"])
         """
 
-        vocab = Vocab()
         # pylint: disable=protected-access
-        vocab._c_vocab = dataset._build_vocab(columns, freq_range, top_k, special_tokens, special_first)
-        vocab._set_unk(special_tokens)
-        vocab._dict = vocab._c_vocab.vocab()
+        if not isinstance(dataset, TextBaseDataset):
+            raise ValueError('dataset must be subclass of TextBaseDataset.')
+
+        ds_vocab = dataset._build_vocab(columns, freq_range, top_k, special_tokens, special_first)
+        vocab = Vocab(ds_vocab.vocab())
 
         return vocab
-
-
-    def lookup_ids(self, tokens):
-        """
-        Converts a token string or a sequence of tokens in a single integer id or a sequence of ids.
-
-        Args:
-            tokens (Union[str, list[str]]): One or several token(s) to convert to token id(s).
-
-        Returns:
-            - list[int], The token id or list of token ids.
-              if only one token used to lookup,
-              return one id instead of a list of ids.
-
-        Examples:
-            >>> import mindspore.dataset.text as text
-            >>> vocab = text.Vocab.from_list(["w1", "w2", "w3"], special_tokens=["<unk>"], special_first=True)
-            >>> ids = vocab.lookup_ids(["w1", "w3"])
-        """
-        if isinstance(tokens, list):
-            for token in tokens:
-                if not token not in self._dict:
-                    raise RuntimeError(f"{token} is not in vocab.")
-        if isinstance(tokens, str):
-            if not tokens not in self._dict:
-                raise RuntimeError(f"{tokens} is not in vocab.")
-            tokens = [tokens]
-        return self.c_vocab.tokens_to_ids(tokens)
-
-    def lookup_tokens(self, ids):
-        """
-        Converts a single index or a sequence of indices in a token or a sequence of tokens.
-        If id does not exist, return empty string.
-
-        Args:
-            ids (Union[int, list[int]]): The token id (or token ids) to convert to tokens.
-
-        Returns:
-            - List<str>, The decoded token(s).
-              if only one id used to lookup,
-              return one token instead of a list of tokens.
-
-        Raises:
-            RuntimeError: If 'ids' is not in vocab.
-
-        Examples:
-            >>> import mindspore.dataset.text as text
-            >>> vocab = text.Vocab.from_list(["w1", "w2", "w3"], special_tokens=["<unk>"], special_first=True)
-            >>> token = vocab.lookup_tokens(0)
-        """
-        if isinstance(ids, list):
-            for idx in ids:
-                if idx not in range(0, len(self._dict)):
-                    raise RuntimeError(f"idx {idx} is not in vocab.")
-        if isinstance(ids, int):
-            if ids not in range(0, len(self._dict)):
-                raise RuntimeError(f"idx {ids} is not in vocab.")
-            ids = [ids]
-        return self.c_vocab.ids_to_tokens(ids)
-
-    def append_token(self, token):
-        r"""
-        Args:
-            token (str): The token used to lookup the corresponding index.
-
-        Raises:
-            RuntimeError: If `token` already exists in the vocab.
-
-        """
-        if isinstance(token, str):
-            if token in self._dict:
-                warnings.warn(f"{token} already exists in the vocab.")
-            else:
-                append_id = len(self._dict)
-                self._dict[token] = append_id
-                self._c_vocab = cde.Vocab.from_dict(self._dict)
-        else:
-            raise TypeError(f"{token} is not a str type.")
-
-    def insert_token(self, token, index):
-        r"""
-        Args:
-            token (str): The token used to lookup the corresponding index.
-            index (int): The index corresponding to the associated token.
-
-        Raises:
-            TypeError: If 'token' is not a str.
-            TypeErrpr: If 'index' is not an int.
-            RuntimeError: If 'token' already exists in the vocab,
-            RuntimeError: If `index` is not in range [0, Vocab.__len__()].
-        """
-        if not isinstance(token, str):
-            raise TypeError(f"token {token} is not str type.")
-        if not isinstance(index, int):
-            raise TypeError(f"index {index} is not int type.")
-        if index not in range(0, len(self._dict)):
-            raise RuntimeError(f"index {index} is out of range [0, {len(self._dict)}]")
-        if token in self._dict:
-            warnings.warn(f"{token} already exists in the vocab.")
-        else:
-            self._dict.update({token: index})
-            self._c_vocab = cde.Vocab.from_dict(self._dict)
 
     @classmethod
     def from_pretrained(cls, name="glove.6B.50d", root=DEFAULT_ROOT,
@@ -281,26 +260,6 @@ class Vocab:
 
         return vocab
 
-    def get_default_index(self):
-        r"""
-        Returns:
-            Value of default index if it is set.
-        """
-        if self._unk_token is None:
-            raise ValueError("Can not find '<unk>' in vocab.")
-        return self._dict.get(self._unk_token)
-
-    def set_default_index(self, index=None):
-        r"""
-        Args:
-            index: Value of default index. This index will be returned when OOV token is queried.
-        """
-        if self._unk_token is not None:
-            warnings.warn("default index has been set, the new setting will be automatically ignored.")
-        else:
-            self._unk_token = '<unk>'
-            self._dict['<unk>'] = index if index is not None else len(self._dict)
-            self._c_vocab = cde.Vocab.from_dict(self._dict)
 
 pretrained_aliases = {
     "glove.6B.50d": "https://huggingface.co/datasets/Aore/MindNLP_vocab/resolve/main/glove.6B.50d.vocab.txt",
@@ -310,3 +269,5 @@ pretrained_aliases = {
     "fasttext": "https://huggingface.co/datasets/Aore/MindNLP_vocab/resolve/main/wiki-news-300d-1M.txt",
     "fasttext-subword": "https://huggingface.co/datasets/Aore/MindNLP_vocab/resolve/main/wiki-news-300d-1M-subword.txt",
 }
+
+__all__ = ['Vocab']
