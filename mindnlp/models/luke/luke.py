3871091041c57061da9758dfaf3c32b7ec270545
Stuprosur <2511002562@qq.com>
2023-03-23 15:54:13 +0800

    add blocks of luke model and modify copyright notice.

--- mindnlp/models/luke/luke.py
+++ mindnlp/models/luke/luke.py
@@ -1,26 +1,30 @@
-# Copyright 2022 Huawei Technologies Co., Ltd
-#
+# coding=utf-8
+# Copyright 2021 The Eleuther AI and HuggingFace Inc. team. All rights reserved.
+# Copyright 2023 Huawei Technologies Co., Ltd
+
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.
+# limitations under the License.\
 # ============================================================================
 """
 MindNlp LUKE model
 """
-# import math
+import math
+
 import mindspore
 import numpy as np
 from mindspore import nn
-from mindspore import ops
+from mindspore import ops, Tensor
 
+from mindnlp.models.luke.luke_config import LukeConfig
 from ..utils import logging
 
 logger = logging.get_logger(__name__)
@@ -31,13 +35,13 @@ class LukeEmbeddings(nn.Cell):
     LukeEmbeddings
     """
 
-    def __init__(self, config):
+    def __init__(self, config: LukeConfig):
         super().__init__()
         self.word_embeddings = nn.Embedding(config.vocab_size, config.hidden_size, padding_idx=config.pad_token_id)
         self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)
         self.token_type_embeddings = nn.Embedding(config.type_vocab_size, config.hidden_size)
         self.layer_norm = nn.LayerNorm([config.hidden_size, ], epsilon=config.layer_norm_eps)
-        self.dropout = nn.Dropout(p=config.hidden_dropout_prob)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
         # End copy
         self.padding_idx = config.pad_token_id
@@ -65,7 +69,7 @@ class LukeEmbeddings(nn.Cell):
             input_shape = inputs_embeds.shape[:-1]
 
         if token_type_ids is None:
-            token_type_ids = mindspore.Tensor(np.zeros(input_shape), dtype=mindspore.int64)
+            token_type_ids = Tensor(np.zeros(input_shape), dtype=mindspore.int64)
 
         if inputs_embeds is None:
             inputs_embeds = self.word_embeddings(input_ids)
@@ -91,6 +95,228 @@ class LukeEmbeddings(nn.Cell):
         return ops.broadcast_to(position_ids.unsqueeze(0), input_shape)
 
 
+class LukeEntityEmbeddings(nn.Cell):
+    """
+    LukeEntityEmbeddings
+    """
+
+    def __init__(self, config: LukeConfig):
+        super().__init__()
+        self.config = config
+
+        self.entity_embeddings = nn.Embedding(config.entity_vocab_size, config.entity_emb_size, padding_idx=0)
+        if config.entity_emb_size != config.hidden_size:
+            self.entity_embedding_dense = nn.Dense(config.entity_emb_size, config.hidden_size, has_bias=False)
+
+        self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)
+        self.token_type_embeddings = nn.Embedding(config.type_vocab_size, config.hidden_size)
+
+        self.layer_norm = nn.LayerNorm([config.hidden_size, ], epsilon=config.layer_norm_eps)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+
+    def construct(
+            self, entity_ids, position_ids, token_type_ids=None
+    ):
+        if token_type_ids is None:
+            token_type_ids = ops.zeros_like(entity_ids)
+
+        entity_embeddings = self.entity_embeddings(entity_ids)
+        if self.config.entity_emb_size != self.config.hidden_size:
+            entity_embeddings = self.entity_embedding_dense(entity_embeddings)
+
+        position_embeddings = self.position_embeddings(ops.clamp(position_ids, min=0))
+        position_embedding_mask = Tensor(position_ids != -1, dtype=position_embeddings.dtype).unsqueeze(-1)
+        position_embeddings = position_embeddings * position_embedding_mask
+        position_embeddings = position_embeddings.sum(axis=-2)
+        position_embeddings = position_embeddings / position_embedding_mask.sum(axis=-2).clamp(min=1e-7)
+
+        token_type_embeddings = self.token_type_embeddings(token_type_ids)
+
+        embeddings = entity_embeddings + position_embeddings + token_type_embeddings
+        embeddings = self.layer_norm(embeddings)
+        embeddings = self.dropout(embeddings)
+
+        return embeddings
+
+
+class LukeSelfAttention(nn.Cell):
+    """
+    LukeSelfAttention
+    """
+
+    def __init__(self, config):
+        super().__init__()
+        if config.hidden_size % config.num_attention_heads != 0 and not hasattr(config, "embedding_size"):
+            raise ValueError(
+                f"The hidden size {config.hidden_size,} is not a multiple of the number of attention "
+                f"heads {config.num_attention_heads}."
+            )
+
+        self.num_attention_heads = config.num_attention_heads
+        self.attention_head_size = int(config.hidden_size / config.num_attention_heads)
+        self.all_head_size = self.num_attention_heads * self.attention_head_size
+        self.use_entity_aware_attention = config.use_entity_aware_attention
+
+        self.query = nn.Dense(config.hidden_size, self.all_head_size)
+        self.key = nn.Dense(config.hidden_size, self.all_head_size)
+        self.value = nn.Dense(config.hidden_size, self.all_head_size)
+
+        if self.use_entity_aware_attention:
+            self.w2e_query = nn.Dense(config.hidden_size, self.all_head_size)
+            self.e2w_query = nn.Dense(config.hidden_size, self.all_head_size)
+            self.e2e_query = nn.Dense(config.hidden_size, self.all_head_size)
+
+        self.dropout = nn.Dropout(config.attention_probs_dropout_prob)
+
+    def transpose_for_scores(self, x):
+        new_x_shape = x.shape[:-1] + (self.num_attention_heads, self.attention_head_size)
+        x = x.view(*new_x_shape)
+        return x.permute(0, 2, 1, 3)
+
+    def construct(
+            self,
+            word_hidden_states,
+            entity_hidden_states,
+            attention_mask=None,
+            head_mask=None,
+            output_attentions=False,
+    ):
+        word_size = word_hidden_states.shape[1]
+
+        if entity_hidden_states is None:
+            concat_hidden_states = word_hidden_states
+        else:
+            concat_hidden_states = ops.cat((word_hidden_states, entity_hidden_states), axis=1)
+
+        key_layer = self.transpose_for_scores(self.key(concat_hidden_states))
+        value_layer = self.transpose_for_scores(self.value(concat_hidden_states))
+
+        if self.use_entity_aware_attention and entity_hidden_states is not None:
+            # compute query vectors using word-word (w2w), word-entity (w2e), entity-word (e2w), entity-entity (e2e)
+            # query layers
+            w2w_query_layer = self.transpose_for_scores(self.query(word_hidden_states))
+            w2e_query_layer = self.transpose_for_scores(self.w2e_query(word_hidden_states))
+            e2w_query_layer = self.transpose_for_scores(self.e2w_query(entity_hidden_states))
+            e2e_query_layer = self.transpose_for_scores(self.e2e_query(entity_hidden_states))
+
+            # compute w2w, w2e, e2w, and e2e key vectors used with the query vectors computed above
+            w2w_key_layer = key_layer[:, :, :word_size, :]
+            e2w_key_layer = key_layer[:, :, :word_size, :]
+            w2e_key_layer = key_layer[:, :, word_size:, :]
+            e2e_key_layer = key_layer[:, :, word_size:, :]
+
+            # compute attention scores based on the dot product between the query and key vectors
+            w2w_attention_scores = ops.matmul(w2w_query_layer, w2w_key_layer.swapaxes(-1, -2))
+            w2e_attention_scores = ops.matmul(w2e_query_layer, w2e_key_layer.swapaxes(-1, -2))
+            e2w_attention_scores = ops.matmul(e2w_query_layer, e2w_key_layer.swapaxes(-1, -2))
+            e2e_attention_scores = ops.matmul(e2e_query_layer, e2e_key_layer.swapaxes(-1, -2))
+
+            # combine attention scores to create the final attention score matrix
+            word_attention_scores = ops.cat([w2w_attention_scores, w2e_attention_scores], axis=3)
+            entity_attention_scores = ops.cat([e2w_attention_scores, e2e_attention_scores], axis=3)
+            attention_scores = ops.cat([word_attention_scores, entity_attention_scores], axis=2)
+
+        else:
+            query_layer = self.transpose_for_scores(self.query(concat_hidden_states))
+            attention_scores = ops.matmul(query_layer, key_layer.swapaxes(-1, -2))
+
+        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+        if attention_mask is not None:
+            # Apply the attention mask is (precomputed for all layers in LukeModel forward() function)
+            attention_scores = attention_scores + attention_mask
+
+        # Normalize the attention scores to probabilities.
+        attention_probs = ops.softmax(attention_scores, axis=-1)
+
+        # This is actually dropping out entire tokens to attend to, which might
+        # seem a bit unusual, but is taken from the original Transformer paper.
+        attention_probs = self.dropout(attention_probs)
+
+        # Mask heads if we want to
+        if head_mask is not None:
+            attention_probs = attention_probs * head_mask
+
+        context_layer = ops.matmul(attention_probs, value_layer)
+
+        context_layer = context_layer.permute(0, 2, 1, 3)
+        new_context_layer_shape = context_layer.shape[:-2] + (self.all_head_size,)
+        context_layer = context_layer.view(*new_context_layer_shape)
+
+        output_word_hidden_states = context_layer[:, :word_size, :]
+        if entity_hidden_states is None:
+            output_entity_hidden_states = None
+        else:
+            output_entity_hidden_states = context_layer[:, word_size:, :]
+
+        if output_attentions:
+            outputs = (output_word_hidden_states, output_entity_hidden_states, attention_probs)
+        else:
+            outputs = (output_word_hidden_states, output_entity_hidden_states)
+
+        return outputs
+
+
+class LukeSelfOutput(nn.Cell):
+    def __init__(self, config):
+        super().__init__()
+        self.dense = nn.Dense(config.hidden_size, config.hidden_size)
+        self.layer_norm = nn.LayerNorm([config.hidden_size, ], epsilon=config.layer_norm_eps)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+
+    def forward(self, hidden_states: Tensor, input_tensor: Tensor) -> Tensor:
+        hidden_states = self.dense(hidden_states)
+        hidden_states = self.dropout(hidden_states)
+        hidden_states = self.LayerNorm(hidden_states + input_tensor)
+        return hidden_states
+
+
+class LukeAttention(nn.Cell):
+    def __init__(self, config):
+        super().__init__()
+        self.self = LukeSelfAttention(config)
+        self.output = LukeSelfOutput(config)
+        self.pruned_heads = set()
+
+    def prune_heads(self, heads):
+        raise NotImplementedError("LUKE does not support the pruning of attention heads")
+
+    def forward(
+            self,
+            word_hidden_states,
+            entity_hidden_states,
+            attention_mask=None,
+            head_mask=None,
+            output_attentions=False,
+    ):
+        word_size = word_hidden_states.shape[1]
+        self_outputs = self.self(
+            word_hidden_states,
+            entity_hidden_states,
+            attention_mask,
+            head_mask,
+            output_attentions,
+        )
+        if entity_hidden_states is None:
+            concat_self_outputs = self_outputs[0]
+            concat_hidden_states = word_hidden_states
+        else:
+            concat_self_outputs = ops.cat(self_outputs[:2], axis=1)
+            concat_hidden_states = ops.cat([word_hidden_states, entity_hidden_states], axis=1)
+
+        attention_output = self.output(concat_self_outputs, concat_hidden_states)
+
+        word_attention_output = attention_output[:, :word_size, :]
+        if entity_hidden_states is None:
+            entity_attention_output = None
+        else:
+            entity_attention_output = attention_output[:, word_size:, :]
+
+        # add attentions if we output them
+        outputs = (word_attention_output, entity_attention_output) + self_outputs[2:]
+
+        return outputs
+
+
 def create_position_ids_from_input_ids(input_ids, padding_idx):
     """
     Replace non-padding symbols with their position numbers. Position numbers begin at padding_idx+1. Padding symbols
@@ -99,3 +325,4 @@ def create_position_ids_from_input_ids(input_ids, padding_idx):
     mask = ops.not_equal(input_ids, padding_idx).astype(mindspore.int32)
     incremental_indices = ops.cumsum(mask, -1).astype(mindspore.int32) * mask
     return incremental_indices.astype(mindspore.int64) + padding_idx
+
--- mindnlp/models/luke/luke_config.py
+++ mindnlp/models/luke/luke_config.py
@@ -1,10 +1,12 @@
-# Copyright 2022 Huawei Technologies Co., Ltd
-#
+# coding=utf-8
+# Copyright 2021 The Eleuther AI and HuggingFace Inc. team. All rights reserved.
+# Copyright 2023 Huawei Technologies Co., Ltd
+
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
