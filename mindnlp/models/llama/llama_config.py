0394bd2a59ac6ab4ae422c73744f98eb8cc36326
khoray <1251433731@qq.com>
2023-03-30 16:56:15 +0800

    add llama model and rmsnorm ut

--- mindnlp/models/llama/llama_config.py
+++ mindnlp/models/llama/llama_config.py
@@ -1,32 +1,44 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# This software may be used and distributed according to the terms of the GNU General Public License version 3.
+# Copyright 2023 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""llama model configuration"""
 
-from mindspore import nn, ops
-from typing import Optional, Tuple
-from dataclasses import dataclass
-import math
+from mindnlp.abc.backbones.pretrained import PretrainedConfig
 
-@dataclass
-class ModelArgs:
-    dim: int = 512
-    n_layers: int = 8
-    n_heads: int = 8
-    vocab_size: int = -1  # defined later by tokenizer
-    multiple_of: int = 256  # make SwiGLU hidden layer size multiple of large power of 2
-    norm_eps: float = 1e-5
+class LlamaConfig(PretrainedConfig):
+    """
+    Configuration for Llama
+    """
+    def __init__(
+            self,
+            dim=512,
+            n_layers=8,
+            n_heads=8,
+            vocab_size=-1,  # defined later by tokenizer
+            multiple_of=256,  # make SwiGLU hidden layer size multiple of large power of 2
+            norm_eps=1e-5,
+            max_batch_size=32,
+            max_seq_len=2048,
+            **kwargs):
+        super().__init__(**kwargs)
+        self.vocab_size = vocab_size
+        self.n_layers = n_layers
+        self.max_seq_len = max_seq_len
+        self.dim = dim
+        self.n_heads = n_heads
+        self.multiple_of = multiple_of
+        self.max_batch_size = max_batch_size
+        self.norm_eps = norm_eps
 
-    max_batch_size: int = 32
-    max_seq_len: int = 2048
-
-class RMSNorm(nn.Cell):
-    def __init__(self, dim: int, eps: float = 1e-6):
-        super().__init__()
-        self.eps = eps
-        self.weight = nn.Parameter(ops.ones(dim))
-
-    def _norm(self, x):
-        return x * ops.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
-
-    def forward(self, x):
-        output = self._norm(x.float()).type_as(x)
-        return output * self.weight
+__all__ = ['LlamaConfig']
