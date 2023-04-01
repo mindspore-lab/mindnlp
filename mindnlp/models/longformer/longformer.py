de5c26bd14310dea86a5df26ea3bd0a55e7d654d
Yicorner <2680015550@qq.com>
2023-04-01 09:48:31 +0800

    LongformerForSequenceClassification

--- mindnlp/models/longformer/longformer.py
+++ mindnlp/models/longformer/longformer.py
@@ -28,7 +28,7 @@ import mindspore
 from mindspore import nn
 from mindspore import Tensor
 from mindspore import ops
-from mindspore.nn import CrossEntropyLoss
+from mindspore.nn import CrossEntropyLoss, MSELoss, BCEWithLogitsLoss
 
 from ..utils.activations import ACT2FN
 from .longformer_config import LongformerConfig
@@ -1665,3 +1665,109 @@ class LongformerForMaskedLM(LongformerPreTrainedModel):
         if not return_dict or return_dict:
             output = (prediction_scores,) + outputs[2:]
             return ((masked_lm_loss,) + output) if masked_lm_loss is not None else output
+
+
+class LongformerForSequenceClassification(LongformerPreTrainedModel):
+
+    _keys_to_ignore_on_load_unexpected = [r"pooler"]
+
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+        self.config = config
+
+        self.longformer = LongformerModel(config, add_pooling_layer=False)
+        self.classifier = LongformerClassificationHead(config)
+
+        # Initialize weights and apply final processing
+        self.post_init()
+
+    # qbh delete docs
+    def construct(
+        self,
+        input_ids: Optional[Tensor] = None,
+        attention_mask: Optional[Tensor] = None,
+        global_attention_mask: Optional[Tensor] = None,
+        head_mask: Optional[Tensor] = None,
+        token_type_ids: Optional[Tensor] = None,
+        position_ids: Optional[Tensor] = None,
+        inputs_embeds: Optional[Tensor] = None,
+        labels: Optional[Tensor] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+    ) -> Tuple:
+        r"""
+        labels (`torch.LongTensor` of shape `(batch_size,)`, *optional*):
+            Labels for computing the sequence classification/regression loss. Indices should be in `[0, ...,
+            config.num_labels - 1]`. If `config.num_labels == 1` a regression loss is computed (Mean-Square loss), If
+            `config.num_labels > 1` a classification loss is computed (Cross-Entropy).
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        if global_attention_mask is None:
+            logger.info("Initializing global attention on CLS token...")
+            global_attention_mask = ops.zeros_like(input_ids)
+            # global attention on cls token
+            global_attention_mask[:, 0] = 1
+
+        outputs = self.longformer(
+            input_ids,
+            attention_mask=attention_mask,
+            global_attention_mask=global_attention_mask,
+            head_mask=head_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+        sequence_output = outputs[0]
+        logits = self.classifier(sequence_output)
+
+        loss = None
+        if labels is not None:
+            if self.config.problem_type is None:
+                if self.num_labels == 1:
+                    self.config.problem_type = "regression"
+                elif self.num_labels > 1 and (labels.dtype == mindspore.int64 or labels.dtype == mindspore.int32):
+                    self.config.problem_type = "single_label_classification"
+                else:
+                    self.config.problem_type = "multi_label_classification"
+
+            if self.config.problem_type == "regression":
+                loss_fct = MSELoss()
+                if self.num_labels == 1:
+                    loss = loss_fct(logits.squeeze(), labels.squeeze())
+                else:
+                    loss = loss_fct(logits, labels)
+            elif self.config.problem_type == "single_label_classification":
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            elif self.config.problem_type == "multi_label_classification":
+                loss_fct = BCEWithLogitsLoss()
+                loss = loss_fct(logits, labels)
+
+        if not return_dict or return_dict:
+            output = (logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+
+class LongformerClassificationHead(nn.Cell):
+    """Head for sentence-level classification tasks."""
+
+    def __init__(self, config):
+        super().__init__()
+        self.dense = nn.Dense(config.hidden_size, config.hidden_size)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.out_proj = nn.Dense(config.hidden_size, config.num_labels)
+
+    def construct(self, hidden_states, **kwargs):
+        hidden_states = hidden_states[:, 0, :]  # take <s> token (equiv. to [CLS])
+        hidden_states = self.dropout(hidden_states)
+        hidden_states = self.dense(hidden_states)
+        hidden_states = mindspore.ops.tanh(hidden_states)
+        hidden_states = self.dropout(hidden_states)
+        output = self.out_proj(hidden_states)
+        return output
--- tests/ut/models/longformer/test_embeddings.py
+++ tests/ut/models/longformer/test_embeddings.py
@@ -31,6 +31,7 @@ from mindnlp.models.longformer.longformer import LongformerPooler
 from mindnlp.models.longformer.longformer import LongformerLMHead
 from mindnlp.models.longformer.longformer import LongformerModel
 from mindnlp.models.longformer.longformer import LongformerForMaskedLM
+from mindnlp.models.longformer.longformer import LongformerForSequenceClassification
 
 
 class TestModelingEmbeddings(unittest.TestCase):
@@ -355,7 +356,7 @@ class TestModelingLongformerModel(unittest.TestCase):
         assert (1, 768) == ms_outputs[1].shape
 
 
-class TestModelingLongformerModel(unittest.TestCase):
+class TestModelingLongformerForMaskedLM(unittest.TestCase):
     r"""
     Test model bert
     """
@@ -389,3 +390,39 @@ class TestModelingLongformerModel(unittest.TestCase):
             global_attention_mask=ms_global_attention_mask,
         )
         assert (1, 10, 30) == ms_outputs[0].shape
+
+
+class TestModelingLongformerForSequenceClassification(unittest.TestCase):
+    r"""
+    Test model bert
+    """
+    def setUp(self):
+        """
+        Set up.
+        """
+        self.input = None
+
+    def test_modeling_longformer_embedding(self):
+        r"""
+        Test model bert with pynative mode
+        """
+        ms_config = LongformerConfig(
+            attention_window=[8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8],
+            max_position_embeddings=40,
+            vocab_size=30,
+        )
+        ms_model = LongformerForSequenceClassification(ms_config)
+        ms_model.set_train(False)
+        input_ids = np.random.randint(1, 10, (1, 10))
+        attention_mask = np.random.randint(0, 2, (1, 10))
+        global_attention_mask = np.random.randint(0, 2, (1, 10))
+
+        ms_input_ids = mindspore.Tensor(input_ids, dtype=mindspore.int32)
+        ms_attention_mask = mindspore.Tensor(attention_mask, dtype=mindspore.int32)
+        ms_global_attention_mask = mindspore.Tensor(global_attention_mask, dtype=mindspore.int32)
+        ms_outputs = ms_model(
+            input_ids=ms_input_ids,
+            attention_mask=ms_attention_mask,
+            global_attention_mask=ms_global_attention_mask,
+        )
+        assert (1, 2) == ms_outputs[0].shape
