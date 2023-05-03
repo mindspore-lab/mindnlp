939ceb30e9a5ed43d00a68248fe1ba2cab99a562
fangzheng20204102 <2413606723@qq.com>
2023-05-03 22:30:08 +0800

    complete glm model

---
+++ mindnlp/models/glm/__init__.py
@@ -0,0 +1,24 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# Copyright 2023 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+# This software may be used and distributed according to the terms of the GNU General Public License version 3.
+from . import glm, glm_config
+from .glm import *
+from .glm_config import *
+
+__all__ = []
+__all__.extend(glm.__all__)
+__all__.extend(glm_config.__all__)
--- mindnlp/models/glm/glm_config.py
+++ mindnlp/models/glm/glm_config.py
@@ -1,31 +1,48 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# Copyright 2023 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
 
-
+# This software may be used and distributed according to the terms of the GNU General Public License version 3.
 """
 GLM Model Config
 """
 
-from mindnlp.abc.backbones.pretrained import PretrainedConfig
+from mindnlp.abc.backbones.pretrained import PreTrainedConfig
+from mindnlp.models.glm import glm
 
-class GLMConfig(PretrainedConfig):
+class GLMConfig(PreTrainedConfig):
     """
     GLMConfig
     """
 
     def __init__(
         self,
-        num_layers,
-        vocab_size,
-        hidden_size,
-        num_attention_heads,
-        embedding_dropout_prob,
-        attention_dropout_prob,
-        output_dropout_prob,
-        max_sequence_length,
-        max_memory_length,
-        checkpoint_activations,
+        num_layers=12,
+        vocab_size=50257,
+        hidden_size=512,
+        num_attention_heads=4,
+        embedding_dropout_prob=0.1,
+        attention_dropout_prob=0.1,
+        output_dropout_prob=0.1,
+        max_sequence_length=100,
+        max_memory_length=0,
+        checkpoint_activations=False,
         checkpoint_num_layers=1,
-        parallel_output=True,
+        init_method_std = 0.02,
         relative_encoding=False,
+        layernorm_epsilon=1.0e-5,
         block_position_encoding=False,
         output_predict=True,
         spell_length=None,
@@ -47,8 +64,9 @@ class GLMConfig(PretrainedConfig):
         self.max_memory_length = max_memory_length
         self.checkpoint_activations = checkpoint_activations
         self.checkpoint_num_layers = checkpoint_num_layers
-        self.parallel_output = parallel_output
+        self.init_method_std = init_method_std
         self.relative_encoding = relative_encoding
+        self.layernorm_epsilon = layernorm_epsilon
         self.block_position_encoding = block_position_encoding
         self.spell_length = spell_length
         self.spell_func = spell_func
