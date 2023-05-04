77877cc7c13c10b7a31c3a4e0d5a737c0053b105
FLoutione <3070240252@qq.com>
2023-05-04 10:53:38 +0800

    add ut_gpt2_tokenizer

---
+++ tests/ut/transforms/test_gpt2_tokenizer.py
@@ -0,0 +1,17 @@
+"""Test the GPT2Tokenizer"""
+
+import mindspore as ms
+from mindspore.dataset import GeneratorDataset
+from mindnlp.transforms import GPT2Tokenizer
+
+def test_gpt2_tokenizer_from_pretrained():
+    """test GPTTokenizer from pretrained."""
+    texts = ['i make a small mistake when i\'m working! 床前明月光']
+    test_dataset = GeneratorDataset(texts, 'text')
+
+    gpt2_tokenizer = GPT2Tokenizer.from_pretrained('gpt2', return_token=True)
+    test_dataset = test_dataset.map(operations=gpt2_tokenizer)
+    dataset_after = next(test_dataset.create_tuple_iterator())[0]
+
+    assert len(dataset_after) == 20
+    assert dataset_after.dtype == ms.string
