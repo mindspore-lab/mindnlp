b84c3d66df0c8263c0ac4f75e3f520a60f29c910
YkChen <1147566753@qq.com>
2023-05-06 21:22:18 +0800

    add_codegen_test_tokenizer

--- mindnlp/models/codegen/codegen_config.py
+++ mindnlp/models/codegen/codegen_config.py
@@ -17,12 +17,32 @@
 """ CodeGen model configuration"""
 
 from mindnlp.abc import PreTrainedConfig
+from mindnlp.configs import MINDNLP_CONFIG_URL_BASE
+
+__all__ = ['CodeGenConfig']
+
+CODEGEN_SUPPORT_LIST = ["Salesforce/codegen-350M-nl",
+                        "Salesforce/codegen-350M-multi",
+                        "Salesforce/codegen-350M-mono",
+                        "Salesforce/codegen-2B-nl",
+                        "Salesforce/codegen-2B-multi",
+                        "Salesforce/codegen-2B-mono",
+                        "Salesforce/codegen-6B-nl",
+                        "Salesforce/codegen-6B-multi",
+                        "Salesforce/codegen-6B-mono",
+                        "Salesforce/codegen-16B-nl",
+                        "Salesforce/codegen-16B-multi",
+                        "Salesforce/codegen-16B-mono"]
+
+CONFIG_ARCHIVE_MAP = {
+    model: MINDNLP_CONFIG_URL_BASE.format('codegen', model) for model in CODEGEN_SUPPORT_LIST}
 
 
 class CodeGenConfig(PreTrainedConfig):
     r"""
     CodeGen config
     """
+    pretrained_config_archive_map = CONFIG_ARCHIVE_MAP
 
     model_type = "codegen"
     attribute_map = {
