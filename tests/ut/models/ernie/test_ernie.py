9b3e06708c385a097a42360785d5b7fc6aeac72b
JinPan-Second <128780655+JinPan-Second@users.noreply.github.com>
2023-05-23 08:13:54 +0800

    ernie model commit ut (#522)

--- tests/ut/models/ernie/test_ernie.py
+++ tests/ut/models/ernie/test_ernie.py
@@ -21,7 +21,18 @@ import mindspore
 from mindspore import Tensor
 from mindnlp.models import (
     UIEConfig,
-    UIE,
+    UIE
+)
+from mindnlp.models.ernie.ernie import (
+    ErnieForSequenceClassification,
+    ErnieForQuestionAnswering,
+    ErnieForTokenClassification,
+    ErnieForMultipleChoice,
+    ErnieLMPredictionHead,
+    ErnieOnlyMLMHead,
+    ErnieForMaskedLM,
+    ErnieForPretraining,
+    ErniePretrainingHeads
 )
 
 
@@ -58,6 +69,99 @@ class TestModelingErnie(unittest.TestCase):
         outputs = model(input_ids=input_ids)
         assert outputs[0].shape == (1, 20)
         assert outputs[1].shape == (1, 20)
+
+    def test_sequenceclassification_model(self):
+        """
+        Test ErnieForSequenceClassification
+        """
+        model = ErnieForSequenceClassification(self.config)
+        input_ids = Tensor(np.random.randint(
+            0, 100, (1, 20)), dtype=mindspore.int32)
+        outputs = model(input_ids=input_ids)
+        assert outputs.shape == (1, 2)
+
+    def test_questionanswering_model(self):
+        """
+        Test ErnieForSequenceClassification
+        """
+        model = ErnieForQuestionAnswering(self.config)
+        input_ids = Tensor(np.random.randint(
+            0, 100, (1, 20)), dtype=mindspore.int32)
+        outputs = model(input_ids=input_ids)
+        assert outputs[0].shape == (1, 20)
+        assert outputs[1].shape == (1, 20)
+
+    def test_tokenclassification_model(self):
+        """
+        Test ErnieForSequenceClassification
+        """
+        model = ErnieForTokenClassification(self.config)
+        input_ids = Tensor(np.random.randint(
+            0, 100, (1, 20)), dtype=mindspore.int32)
+        outputs = model(input_ids=input_ids)
+        assert outputs.shape == (1, 20, 2)
+
+    def test_lmpredictionhead_model(self):
+        """
+        Test LMPredictionHead
+        """
+        model = ErnieLMPredictionHead(self.config)
+        input_ids = Tensor(np.random.randint(
+            0, 100, (128, 128)), dtype=mindspore.float32)
+        outputs = model(input_ids)
+        assert outputs.shape == (128, 1000)
+
+    def test_erniepretrainingheads_model(self):
+        """
+        Test ErniePretrainingHeads
+        """
+
+        model = ErniePretrainingHeads(self.config)
+        input_ids = Tensor(np.random.randint(
+            0, 100, (128, 128)), dtype=mindspore.float32)
+        outputs = model(input_ids,input_ids)
+        assert outputs[0].shape == (128,1000)
+        assert outputs[1].shape == (128,2)
+
+    def test_ernieonlymlmhead_model(self):
+        """
+        Test ErnieOnlyMLMHead
+        """
+
+        model = ErnieOnlyMLMHead(self.config)
+        input_ids = Tensor(np.random.randint(
+            0, 100, (128,128)), dtype=mindspore.float32)
+        outputs = model(input_ids)
+        assert outputs.shape == (128,1000)
+
+    def test_ernieformaskedlm_model(self):
+        """
+        Test ErnieForMaskedLM
+        """
+        model = ErnieForMaskedLM(self.config)
+        input_np = Tensor(np.random.randint(0,100, (1,200)),dtype=mindspore.int32)
+        outputs = model(input_np)
+        assert outputs.shape == (1,200,1000)
+
+    def test_ernieformultiplechoice_model(self):
+        """
+        Test ErnieForMultipleChoice
+        """
+        model = ErnieForMultipleChoice(self.config)
+        input_np = Tensor(np.random.randint(0,100, (10,10,20)),dtype=mindspore.int32)
+        outputs = model(input_np)
+        assert outputs.shape == (50,2)
+
+    def test_erniegorpretraining_model(self):
+        """
+        Test ErnieForPretraining
+        """
+        model = ErnieForPretraining(self.config)
+        input_ids = Tensor(np.random.randint(
+            0, 100, (1,128)), dtype=mindspore.int32)
+        outputs = model(input_ids,input_ids)
+        assert outputs[0].shape == (1,128,1000)
+        assert outputs[1].shape == (1,2)
 
     @pytest.mark.download
     def test_from_pretrained(self):
