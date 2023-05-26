80c54e657c268a34dbee3cf1a56491a07d0aee43
kuoyee <838154900@qq.com>
2023-05-26 18:29:13 +0800

    update xlm code

--- mindnlp/abc/configs/pretrained_config.py
+++ mindnlp/abc/configs/pretrained_config.py
@@ -232,4 +232,3 @@ class PreTrainedConfig:
     def to_json_string(self):
         """Serializes this instance to a JSON string."""
         return json.dumps(self.to_dict(), indent=2, sort_keys=True) + "\n"
-
