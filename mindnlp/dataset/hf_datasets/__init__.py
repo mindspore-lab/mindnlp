1fb754fbbd842da2cb5453f1a04e4b4bbf1730e1
xiaoyi3 <1730499712@qq.com>
2023-06-01 20:41:14 +0800

    add hf_squad2.py

--- mindnlp/dataset/hf_datasets/__init__.py
+++ mindnlp/dataset/hf_datasets/__init__.py
@@ -19,3 +19,5 @@ Directly load huggingface datasets
 from .hf_imdb import HF_IMDB, HF_IMDB_Process
 from .hf_glue import HF_GLUE
 from .hf_msra_ner import HF_Msra_ner, HF_Msra_ner_Process
+from .hf_ptb_text_only import HF_Ptb_text_only, HF_Ptb_text_only_Process
+from .hf_squad2 import HF_SQuAD2
