a2288b9c482c45557e3871e6882b8370a7fb0f33
dyz <32426751+DongyaoZhu@users.noreply.github.com>
2023-06-03 20:44:42 +0800

    roberta arange: numpy not ops

--- mindnlp/models/roberta/roberta.py
+++ mindnlp/models/roberta/roberta.py
@@ -40,7 +40,7 @@ class RobertaEmbeddings(BertEmbeddings):
     def construct(self, input_ids, token_type_ids=None, position_ids=None):
         seq_length = input_ids.shape[1]
         if position_ids is None:
-            position_ids = ops.arange(self.padding_idx + 1, seq_length + self.padding_idx+1, dtype=mindspore.int64)
+            position_ids = mindspore.numpy.arange(self.padding_idx + 1, seq_length + self.padding_idx+1, dtype=mindspore.int64)
             position_ids = position_ids.expand_dims(0).expand_as(input_ids)
         return super().construct(input_ids, token_type_ids, position_ids)
 
