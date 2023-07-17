4a94cad546ac5cfa6a0b3cf11c3e404550b86ce2
qinchen2023 <qinchen@isrc.iscas.ac.cn>
2023-07-18 02:05:29 +0800

    add hf_duconv

--- mindnlp/dataset/hf_datasets/hf_duconv.py
+++ mindnlp/dataset/hf_datasets/hf_duconv.py
@@ -18,16 +18,16 @@ DUcONV load function
 import os
 import json
 from typing import Tuple, Union
-import numpy as np
-import mindspore
-import mindspore.dataset as ds
+
 from mindspore.dataset import GeneratorDataset, transforms
 from mindnlp.utils.download import cache_file
 from mindnlp.dataset.register import load_dataset, process
-from mindnlp.vocab import Vocab
+
 from mindnlp.configs import DEFAULT_ROOT
-from mindnlp.transforms import BasicTokenizer, Lookup
+from mindnlp.transforms import Lookup, Truncate
 from mindnlp.utils import unzip
+
+from mindnlp.dataset.utils import make_bucket
 
 URL = "https://bj.bcebos.com/paddlenlp/datasets/DuConv.zip"
 
@@ -74,7 +74,7 @@ class Duconv:
         return len(self._response)
 
 @load_dataset.register
-def hf_duconv(root=DEFAULT_ROOT, \
+def hf_duconv(root:str=DEFAULT_ROOT, \
               split: Union[Tuple[str], str] = ('train', 'dev','test_1','test_2'),proxies=None):
     r'''
     Load the DuConv dataset
@@ -107,15 +107,12 @@ def hf_duconv(root=DEFAULT_ROOT, \
     datasets_list = []
     cache_dir=os.path.join(root, "datasets", "DuConv")
     if isinstance(split, str):
-        mode_list.append(split)
-    else :
-        for s_name in split:
-            mode_list.append(s_name)
+        mode_list.append(cache_dir + '/DuConv/' + split + '.txt')
     file_path, _ = cache_file(None, cache_dir=cache_dir, url=URL,
                               download_file_name="DuConv.zip", proxies=proxies)
     unzip(file_path,cache_dir)
-    for every_s in split:
-        mode_list.append(cache_dir + '/DuConv/' + every_s + '.txt')
+    for split_item in split:
+        mode_list.append(cache_dir + '/DuConv/' + split_item + '.txt')
     for _, every_ds in enumerate(mode_list):
         dataset = GeneratorDataset(source = Duconv(every_ds),\
         column_names=["id" ,"goal", "knowledge", "conversation", "history","response"],\
@@ -126,172 +123,41 @@ def hf_duconv(root=DEFAULT_ROOT, \
     return datasets_list
 
 @process.register
-def hfduconv_process(dataset, char_vocab, word_vocab=None,\
-                   tokenizer=BasicTokenizer(True),\
-                   max_context_len=768, max_question_len=64, max_char_len=48,\
-                   batch_size=64, drop_remainder=False) -> None:
+def hf_duconv_process(dataset, tokenizer, vocab, batch_size=64, max_context_len=1000, max_qa_len=30,
+                      bucket_boundaries=None, drop_remainder=False)->None:
     """
-    the process of the squad1 dataset
+    the process of the duconv dataset
 
-    Args:
-        dataset (GeneratorDataset): Duconv dataset.
-        tokenizer (TextTensorOperation): Tokenizer you choose to tokenize the text dataset.
-        word_vocab (Vocab): Vocabulary object of words, used to store the mapping of the token and index.
-        char_vocab (Vocab): Vocabulary object of chars, used to store the mapping of the token and index.
-        max_context_len (int): Max length of the context. Default: 768.
-        max_question_len (int): Max length of the question. Default: 64.
-        max_char_len (int): Max length of the char. Default: 48.
-        batch_size (int): The number of rows each batch is created with. Default: 64.
-        drop_remainder (bool): When the last batch of data contains a data entry smaller than batch_size, whether
-            to discard the batch and not pass it to the next operation. Default: False.
-
-    Returns:
-        - MapDataset, duconv Dataset after process.
-
-    Raises:
-        TypeError: If `word_vocab` is not of type text.Vocab.
-        TypeError: If `char_vocab` is not of type text.Vocab.
-        TypeError: If `max_context_len` is not of type int.
-        TypeError: If `max_question_len` is not of type int.
-        TypeError: If `max_char_len` is not of type int.
-        TypeError: If `batch_size` is not of type int.
-        TypeError: If `drop_remainder` is not of type bool.
-
-    Examples:
-        >>> from mindspore.dataset import text
-        >>> from mindnlp.dataset import hf_duconv, hfduconv_process
-        >>> char_dic = {"<unk>": 0, "<pad>": 1, "e": 2, "t": 3, "a": 4, "i": 5, "n": 6,\
-                    "o": 7, "s": 8, "r": 9, "h": 10, "l": 11, "d": 12, "c": 13, "u": 14,\
-                    "m": 15, "f": 16, "p": 17, "g": 18, "w": 19, "y": 20, "b": 21, ",": 22,\
-                    "v": 23, ".": 24, "k": 25, "1": 26, "0": 27, "x": 28, "2": 29, "\"": 30, \
-                    "-": 31, "j": 32, "9": 33, "'": 34, ")": 35, "(": 36, "?": 37, "z": 38,\
-                    "5": 39, "8": 40, "q": 41, "3": 42, "4": 43, "7": 44, "6": 45, ";": 46,\
-                    ":": 47, "\u2013": 48, "%": 49, "/": 50, "]": 51, "[": 52}
-        >>> char_vocab = text.Vocab.from_dict(char_dic)
-        >>> dev_dataset = HF_duconv(split='dev')
-        >>> duconv_dev = hfduconv_process(dataset=dev_dataset, char_vocab=char_vocab)
-        >>> duconv_dev = duconv_dev.create_tuple_iterator()
-        >>> print(next(duconv_dev))
     """
-    c_char_list = []
-    q_char_list = []
-    c_lens = []
-    q_lens = []
-    s_idx = []
-    e_idx = []
-    pad_value_char = char_vocab.lookup_ids("<pad>")
-    abnormals = [' ', '\n', '\u3000', '\u202f', '\u2009','\u200B', '\u0303', '\u092e']
-    for data in dataset:
-        context = data[1].asnumpy().tolist()
-        question = data[2].asnumpy().tolist()
-        answer = data[3].asnumpy().tolist()
-        c_token = tokenizer(context)
-        c_len = len(c_token)
-        q_token = tokenizer(question)
-        q_len = len(q_token)
-        answer_token = tokenizer(answer)
-        answer_len = len(answer_token)
-        s_index = int(data[4])
-        e_index = s_index + len(answer)
-        c_char = []
-        q_char = []
-        # find the starting and ending position of the answer
-        line = 0
-        s_found = False
-        for line, token in enumerate(c_token):
-            while line < len(context):
-                if context[line] in abnormals:
-                    line += 1
-                else:
-                    break
+    pad_value = vocab.tokens_to_ids('<pad>')
+    lookup_op = Lookup(vocab, unk_token='<unk>')
 
-            line += len(token)
-            if line > s_index and s_found is False:
-                s_index = i
-                s_found = True
-            if line >= e_index:
-                e_index = i
-                break
-        # exceptional cases
-        if s_index >= c_len or e_index >= c_len:
-            for i, token in enumerate(c_token):
-                if token == answer_token[0]:
-                    s_index = i
-                    if c_token[i + answer_len - 1] == answer_token[-1]:
-                        e_index = i + answer_len - 1
-                        break
-        # define lookup operation in char vocab
-        char_lookup = Lookup(char_vocab, unk_token="<unk>")
-        # generate the char list of the context(after lookup and padding operation)
-        for token in c_token:
-            token_ids = char_lookup(list(token))
-            token_ids = list(token_ids)
-            if isinstance(token_ids, int):
-                token_list = []
-                token_list.append(token_ids)
-                token_ids = token_list
-            pad_char = transforms.PadEnd(pad_shape=[max_char_len], pad_value=pad_value_char)
-            token_pad = pad_char(token_ids)
-            token_pad = np.array(token_pad, dtype=np.int32)
-            c_char.append(token_pad)
+    dataset = dataset.map([tokenizer, lookup_op], 'goal')
+    dataset = dataset.map([tokenizer, lookup_op], 'knowledge')
+    dataset = dataset.map([tokenizer, lookup_op], 'conversation')
 
-        # generate the char list of the question(after lookup and padding operation)
-        for token in q_token:
-            token_ids = char_lookup(list(token))
-            token_ids = list(token_ids)
-            if isinstance(token_ids, int):
-            # if type(token_ids)==int:
-                token_list = []
-                token_list.append(token_ids)
-                token_ids = token_list
-            pad_char = transforms.PadEnd(pad_shape=[max_char_len], pad_value=pad_value_char)
-            token_pad = pad_char(token_ids)
-            token_pad = np.array(token_pad, dtype=np.int32)
-            q_char.append(token_pad)
+    pad_qa_op = transforms.PadEnd([max_qa_len], pad_value)
+    dataset = dataset.map([pad_qa_op], 'knowledge')
+    dataset = dataset.map([pad_qa_op], 'conversation')
 
-        c_lens.append(c_len)
-        q_lens.append(q_len)
-        s_idx.append(s_index)
-        e_idx.append(e_index)
-        c_char_list.append(c_char)
-        q_char_list.append(q_char)
+    if bucket_boundaries is not None:
+        if not isinstance(bucket_boundaries, list):
+            raise ValueError(f"'bucket_boundaries' must be a list of int, but get {type(bucket_boundaries)}")
+        trancate_context_op = Truncate(max_context_len)
+        dataset = dataset.map([trancate_context_op], 'goal')
+        trancate_qa_op = Truncate(max_qa_len)
+        dataset = dataset.map([trancate_qa_op], 'knowledge')
+        dataset = dataset.map([trancate_qa_op], 'conversation')
 
-    data = (c_char_list, q_char_list, c_lens, q_lens, s_idx, e_idx)
-    dataset2 = ds.NumpySlicesDataset(data=data, column_names=["c_char", "q_char", "c_lens",\
-                                     "q_lens", "s_idx", "e_idx"], shuffle=False)
+        if bucket_boundaries[-1] < max_context_len + 1:
+            bucket_boundaries.append(max_context_len + 1)
+        bucket_batch_sizes = [batch_size] * (len(bucket_boundaries) + 1)
+        dataset = make_bucket(dataset, 'goal', pad_value,
+                              bucket_boundaries, bucket_batch_sizes, drop_remainder)
+    else:
+        pad_context_op = transforms.PadEnd([max_context_len], pad_value)
+        dataset = dataset.map([pad_context_op], 'goal')
 
-    dataset = dataset.zip(dataset2)
-    dataset = dataset.rename(input_columns="id", output_columns="ids")
-    columns_to_project = ["ids", "context", "question", "c_char", "q_char", "c_lens", "q_lens", "s_idx", "e_idx"]
-    dataset = dataset.project(columns=columns_to_project)
+        dataset = dataset.batch(batch_size, drop_remainder=drop_remainder)
 
-    dataset = dataset.map(tokenizer, 'context', 'c_word')
-    dataset = dataset.map(tokenizer, 'question', 'q_word')
-
-    if word_vocab is None:
-        word_vocab = Vocab.from_dataset(dataset, columns=['c_word', 'q_word'],\
-                                             special_tokens=["<unk>", "<pad>"], special_first=True)
-
-    # lookup_op = text.Lookup(word_vocab, unknown_token='<unk>')
-    lookup_op = Lookup(word_vocab, unk_token="<unk>")
-    type_cast_op = transforms.TypeCast(mindspore.int32)
-    pad_value_word = word_vocab.lookup_ids('<pad>')
-
-    dataset = dataset.map(lookup_op, 'c_word')
-    dataset = dataset.map(lookup_op, 'q_word')
-    dataset = dataset.map(type_cast_op, 'c_lens')
-    dataset = dataset.map(type_cast_op, 'q_lens')
-    dataset = dataset.map(type_cast_op, 's_idx')
-    dataset = dataset.map(type_cast_op, 'e_idx')
-
-    pad_op_context = transforms.PadEnd([max_context_len], pad_value_word)
-    dataset = dataset.map([pad_op_context], 'c_word')
-    pad_op_question = transforms.PadEnd([max_question_len], pad_value_word)
-    dataset = dataset.map([pad_op_question], 'q_word')
-    pad_char_context = transforms.PadEnd([max_context_len, max_char_len], pad_value_word)
-    dataset = dataset.map([pad_char_context], 'c_char')
-    pad_char_question = transforms.PadEnd([max_question_len, max_char_len], pad_value_word)
-    dataset = dataset.map([pad_char_question], 'q_char')
-
-    dataset = dataset.batch(batch_size, drop_remainder=drop_remainder)
     return dataset
--- tests/ut/dataset/test_hfduconv.py
+++ tests/ut/dataset/test_hfduconv.py
@@ -20,9 +20,18 @@ import os
 import shutil
 import unittest
 import pytest
-from mindnlp import load_dataset
+from mindnlp import load_dataset, Vocab
 from mindnlp.dataset import hf_duconv
 
+
+char_dic = {"<unk>": 0, "<pad>": 1, "e": 2, "t": 3, "a": 4, "i": 5, "n": 6,\
+                    "o": 7, "s": 8, "r": 9, "h": 10, "l": 11, "d": 12, "c": 13, "u": 14,\
+                    "m": 15, "f": 16, "p": 17, "g": 18, "w": 19, "y": 20, "b": 21, ",": 22,\
+                    "v": 23, ".": 24, "k": 25, "1": 26, "0": 27, "x": 28, "2": 29, "\"": 30, \
+                    "-": 31, "j": 32, "9": 33, "'": 34, ")": 35, "(": 36, "?": 37, "z": 38,\
+                    "5": 39, "8": 40, "q": 41, "3": 42, "4": 43, "7": 44, "6": 45, ";": 46,\
+                    ":": 47, "\u2013": 48, "%": 49, "/": 50, "]": 51, "[": 52}
+char_vocab = Vocab(char_dic)
 
 
 class TestDuConv(unittest.TestCase):
@@ -40,7 +49,7 @@ class TestDuConv(unittest.TestCase):
 
     @pytest.mark.download
     @pytest.mark.local
-    def test_duconv(self):
+    def test_hf_duconv(self):
         """Test DuConv"""
         num_lines = {
             "train": 19900,
@@ -67,5 +76,4 @@ class TestDuConv(unittest.TestCase):
     @pytest.mark.download
     def test_duconv_by_register(self):
         """test hf_duconv by register"""
-        _ = load_dataset('hf_duconv', root=self.root, split='dev')
-        
+        _ = load_dataset('hf_duconv', root=self.root, split=('train','dev','test_1','test_2'))
