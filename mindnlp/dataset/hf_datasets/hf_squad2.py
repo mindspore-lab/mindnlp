a747efa87bf24ef5a12ff469001723a2a4988907
xiaoyi3 <1730499712@qq.com>
2023-07-17 16:14:24 +0800

    add hf_squad2, test

--- mindnlp/dataset/hf_datasets/hf_squad2.py
+++ mindnlp/dataset/hf_datasets/hf_squad2.py
@@ -19,16 +19,23 @@ Hugging Face SQuAD2 load function
 
 import os
 from typing import Union, Tuple
+
+import mindspore
 from datasets import load_dataset as hf_load
-from mindspore.dataset import GeneratorDataset
-from mindnlp.dataset.register import load_dataset
+from mindnlp.transforms import Lookup, Truncate
+from mindspore.dataset import GeneratorDataset, transforms
+from mindnlp.dataset.register import load_dataset, process
+from mindspore.dataset.text import Vocab
+from mindnlp.dataset.utils import make_bucket
 from mindnlp.configs import DEFAULT_ROOT
+import numpy as np
 
 
-class HFsquad2:
+class HFSquad2:
     """
     Hugging Face squad_v2 dataset source
     """
+
     def __init__(self, dataset_list) -> None:
         self.dataset_list = dataset_list
         self._context, self._question = [], []
@@ -40,10 +47,13 @@ class HFsquad2:
             self._context.append(every_dict['context'])
             self._question.append(every_dict['question'])
             self._anwsers.append(every_dict['answers']['text'])
-            self._answers_start.append(every_dict['answers']['answer_start'])
+            if len(every_dict['answers']['answer_start']) == 0:
+                self._answers_start.append(-1)
+            else:
+                self._answers_start.append(every_dict['answers']['answer_start'][0])
 
     def __getitem__(self, index):
-        return self._context[index], self._question[index],\
+        return self._context[index], self._question[index], \
             self._anwsers[index], self._answers_start[index]
 
     def __len__(self):
@@ -52,9 +62,9 @@ class HFsquad2:
 
 @load_dataset.register
 def HF_SQuAD2(
-    root: str = DEFAULT_ROOT,
-    split: Union[Tuple[str], str] = ("train", "validation"),
-    shuffle=True,
+        root: str = DEFAULT_ROOT,
+        split: Union[Tuple[str], str] = ("train", "validation"),
+        shuffle=True,
 ):
     r"""
     Load the SQuAD2 dataset
@@ -76,7 +86,7 @@ def HF_SQuAD2(
     Examples:
         >>> root = "~/.mindnlp"
         >>> split = ('train', 'validation')
-        >>> dataset_train, dataset_validation = HF_SQuAD2(root, split)
+        >>> dataset_train, dataset_validation = HF_SQuAD2(root, split, shuffle=False)
         >>> train_iter = dataset_train.create_tuple_iterator()
         >>> print(next(train_iter))
         [Tensor(shape=[], dtype=String, value= 'Beyoncé Giselle Knowles-Carter...),
@@ -100,9 +110,75 @@ def HF_SQuAD2(
     ds_list = hf_load('squad_v2', split=mode_list, data_dir=cache_dir)
     for every_ds in ds_list:
         datasets_list.append(GeneratorDataset(
-            source=HFsquad2(every_ds),
+            source=HFSquad2(every_ds),
             column_names=column_names, shuffle=shuffle)
         )
     if len(mode_list) == 1:
         return datasets_list[0]
     return datasets_list
+
+
+@process.register
+def HF_SQuAD2_Process(dataset, tokenizer, vocab, batch_size=64, max_context_len=1000, max_qa_len=30,
+                      bucket_boundaries=None, drop_remainder=False):
+    """
+    the process of the SQuAD2 dataset
+
+    Args:
+        dataset (GeneratorDataset): SQuAD2 dataset.
+        tokenizer (TextTensorOperation): tokenizer you choose to tokenize the text dataset.
+        vocab (Vocab): vocabulary object, used to store the mapping of token and index.
+        batch_size (int): size of the batch.
+        max_context_len (int): max length of the context.
+        max_qa_len (int): max length of the quention and answer.
+        bucket_boundaries (list[int]): A list consisting of the upper boundaries of the buckets.
+        drop_remainder (bool): If True, will drop the last batch for each bucket if it is not a full batch
+
+    Returns:
+        - **dataset** (MapDataset) - dataset after transforms.
+
+    Raises:
+        TypeError: If `input_column` is not a string.
+
+    Examples:
+        >>> dataset_train = HF_SQuAD2(split = ('train',))
+        >>> tokenizer = mindnlp.transforms.BasicTokenizer(True)
+        >>> dataset_train = dataset_train.map([tokenizer], 'context')
+        >>> vocab = mindspore.dataset.text.Vocab.from_dataset(dataset_train, 'context', special_tokens=
+        >>>   ['<pad>', '<unk>'], special_first= True)
+        >>> squad2_train = HF_SQuAD2_Process(dataset_train, tokenizer=tokenizer, vocab=vocab, \
+                        bucket_boundaries=[400, 500], max_len=1000, drop_remainder=True)
+    """
+
+    pad_value = vocab.tokens_to_ids('<pad>')
+    lookup_op = Lookup(vocab, unk_token='<unk>')
+
+    dataset = dataset.map([tokenizer, lookup_op], 'context')
+    dataset = dataset.map([tokenizer, lookup_op], 'question')
+    dataset = dataset.map([tokenizer, lookup_op], 'answers')
+
+    pad_qa_op = transforms.PadEnd([max_qa_len], pad_value)
+    dataset = dataset.map([pad_qa_op], 'question')
+    dataset = dataset.map([pad_qa_op], 'answers')
+
+    if bucket_boundaries is not None:
+        if not isinstance(bucket_boundaries, list):
+            raise ValueError(f"'bucket_boundaries' must be a list of int, but get {type(bucket_boundaries)}")
+        trancate_context_op = Truncate(max_context_len)
+        dataset = dataset.map([trancate_context_op], 'context')
+        trancate_qa_op = Truncate(max_qa_len)
+        dataset = dataset.map([trancate_qa_op], 'question')
+        dataset = dataset.map([trancate_qa_op], 'answers')
+
+        if bucket_boundaries[-1] < max_context_len + 1:
+            bucket_boundaries.append(max_context_len + 1)
+        bucket_batch_sizes = [batch_size] * (len(bucket_boundaries) + 1)
+        dataset = make_bucket(dataset, 'context', pad_value,
+                              bucket_boundaries, bucket_batch_sizes, drop_remainder)
+    else:
+        pad_context_op = transforms.PadEnd([max_context_len], pad_value)
+        dataset = dataset.map([pad_context_op], 'context')
+
+        dataset = dataset.batch(batch_size, drop_remainder=drop_remainder)
+
+    return dataset
