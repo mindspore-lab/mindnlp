63e2a7c4ac5e459459dcf10df8d263be1cbc98f9
BraveSeeker <202419330@qq.com>
2023-07-19 19:43:46 +0800

    repair some bug of pylint in moss

--- tests/ut/models/moss/test_modeling_moss.py
+++ tests/ut/models/moss/test_modeling_moss.py
@@ -12,7 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""Test Bert functions"""
+"""
+Test Moss functions
+"""
 import unittest
 import numpy as np
 
