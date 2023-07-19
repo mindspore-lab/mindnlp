c58a5e163319b166180920c2e5c7e2f9264479fc
BraveSeeker <202419330@qq.com>
2023-07-19 19:41:03 +0800

    repair in ut test for moss

--- tests/ut/models/moss/test_modeling_moss.py
+++ tests/ut/models/moss/test_modeling_moss.py
@@ -21,6 +21,8 @@ import mindspore
 from mindspore import Tensor
 
 from mindnlp.models.moss import MossConfig, moss
+
+
 class TestModelingMoss(unittest.TestCase):
     r"""
     Test Moss
@@ -35,70 +37,68 @@ class TestModelingMoss(unittest.TestCase):
                                  n_layer=2,
                                  n_head=8,
                                  n_embd=512)
-    def test_MossAttention(self):
+
+    def test_moss_attention(self):
         r"""
         Test MossAttention
         """
         model = moss.MossAttention(self.config)
 
-        hidden_states = Tensor(np.random.randint(1, 16, (2,512,self.config.n_embd)), dtype=mindspore.float32)
+        hidden_states = Tensor(np.random.randint(1, 16, (2, 512, self.config.n_embd)), dtype=mindspore.float32)
 
         position_ids = Tensor(np.random.randint(0, 1, (2, self.config.n_embd)), dtype=mindspore.int64)
-        
-        attn_output, _ = model( position_ids=position_ids,hidden_states=hidden_states,output_attentions=False)
+
+        attn_output, _ = model(position_ids=position_ids, hidden_states=hidden_states, output_attentions=False)
         assert attn_output.shape == (2, 512, self.config.n_embd)
-    
-    def test_MossMLP(self):
+
+    def test_moss_mlp(self):
         r"""
         Test MossMLP
         """
         intermediate_size = 4096
-        
+
         model = moss.MossMLP(intermediate_size, self.config)
-        
-        hidden_states = Tensor(np.random.randint(1, 16, (2,512,self.config.n_embd)), dtype=mindspore.float32)
-        
-        mlp_out = model( hidden_states=hidden_states)
+
+        hidden_states = Tensor(np.random.randint(1, 16, (2, 512, self.config.n_embd)), dtype=mindspore.float32)
+
+        mlp_out = model(hidden_states=hidden_states)
 
         assert mlp_out.shape == (2, 512, self.config.n_embd)
-    
-  
-    def test_MossBlock(self):
+
+    def test_moss_block(self):
         r"""
         Test MossBlock
         """
         model = moss.MossBlock(self.config)
-        
-        hidden_states = Tensor(np.random.randint(1, 16, (2,512,self.config.n_embd)), dtype=mindspore.float32)
-        position_ids = Tensor(np.random.randint(0, 1, (2,self.config.n_embd)), dtype=mindspore.int64)
-        
-        block_out = model( position_ids=position_ids,hidden_states=hidden_states)[0]
-        
+
+        hidden_states = Tensor(np.random.randint(1, 16, (2, 512, self.config.n_embd)), dtype=mindspore.float32)
+        position_ids = Tensor(np.random.randint(0, 1, (2, self.config.n_embd)), dtype=mindspore.int64)
+
+        block_out = model(position_ids=position_ids, hidden_states=hidden_states)[0]
+
         assert block_out.shape == (2, 512, self.config.n_embd)
-        
- 
-    def test_MossModel(self):
+
+    def test_moss_model(self):
         r"""
         Test MossModel
         """
         model = moss.MossModel(self.config)
-          
+
         ms_input = Tensor(np.random.randint(0, 512, (2, 512)))
-        
+
         model_out = model(input_ids=ms_input)[0]
-        
+
         assert model_out.shape == (2, 512, 512)
 
-
-    def test_MossForCausalLM(self):
+    def test_moss_for_causal_lm(self):
         r"""
         Test MossForCausalLM
         """
         model = moss.MossForCausalLM(self.config)
-          
+
         ms_input = Tensor(np.random.randn(2), dtype=mindspore.int64)
 
         model_out = model(input_ids=ms_input)
-        
+
         assert model_out[0].shape == (2, self.config.vocab_size)
         assert model_out[1][0][0].shape == (1, 8, 2, 512)
