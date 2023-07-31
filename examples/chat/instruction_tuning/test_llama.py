e260e0097876039ff8afafba206005c8fe8f5022
cjl99 <cj2559@163.com>
2023-07-31 13:32:32 +0800

    first commit for chat task in mindnlp

---
+++ examples/chat/instruction_tuning/test_llama.py
@@ -0,0 +1,28 @@
+import os
+
+import mindspore
+from mindspore.dataset import text, GeneratorDataset, transforms
+from mindspore import nn
+
+from mindnlp import load_dataset
+from mindnlp.transforms import PadTransform, GPTTokenizer
+
+from mindnlp.engine import Trainer, Evaluator
+from mindnlp.engine.callbacks import CheckpointCallback, BestModelCallback
+from mindnlp.metrics import Accuracy
+
+
+from mindnlp.models.llama import LlamaConfig, LlamaForCausalLM
+
+
+## eval mmlu
+dataset_test = load_dataset('mmlu', splits='test')
+metric = Accuracy()
+
+model = LlamaForCausalLM.from_pretrained('mindnlp/llama-base')
+
+evaluator = Evaluator(
+    network=model, 
+    eval_dataset=dataset_test, 
+    metrics=metric
+)
