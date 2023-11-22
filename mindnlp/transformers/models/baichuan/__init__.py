11961e10ae37814601b05ef330323d4742c18c6f
Stuprosur <2511002562@qq.com>
2023-11-22 15:20:10 +0800

    fix pylint error

--- mindnlp/transformers/models/baichuan/__init__.py
+++ mindnlp/transformers/models/baichuan/__init__.py
@@ -15,13 +15,10 @@
 """
 BaiChuan Model.
 """
-from . import baichuan, baichuan_config, tokenization_baichuan
 
 from .baichuan import *
 from .baichuan_config import *
-from .tokenization_baichuan import *
 
-__all__= []
+__all__ = []
 __all__.extend(baichuan.__all__)
 __all__.extend(baichuan_config.__all__)
-# __all__.extend(tokenization_baichuan.__all__)
