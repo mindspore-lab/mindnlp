fa24036759ca669da607da39b34bf0f5aa3703ba
woody <1337723542@qq.com>
2024-01-19 22:59:13 +0800

    add mbart model

--- mindnlp/transformers/models/mbart/modeling_mbart.py
+++ mindnlp/transformers/models/mbart/modeling_mbart.py
@@ -469,19 +469,19 @@ class MBartPreTrainedModel(PreTrainedModel):
     supports_gradient_checkpointing = True
     _no_split_modules = ["MBartDecoderLayer", "MBartAttention"]
 
-    def _init_weights(self, module):
+    def _init_weights(self, cell):
         std = self.config.init_std
-        if isinstance(module, nn.Dense):
-            module.weight.set_data(initializer(Normal(self.config.init_std),
-                                               module.weight.shape, module.weight.dtype))
-            if module.bias is not None:
-                module.bias.set_data(ops.zeros_like(module.bias))
-        elif isinstance(module, nn.Embedding):
-            module.weight.set_data(initializer(Normal(
-                sigma=std, mean=0.0), module.weight.shape, module.weight.dtype))
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx] = ops.zeros_like(
-                    module.weight.data[module.padding_idx])
+        if isinstance(cell, nn.Dense):
+            cell.weight.set_data(initializer(Normal(self.config.init_std),
+                                               cell.weight.shape, cell.weight.dtype))
+            if cell.bias is not None:
+                cell.bias.set_data(ops.zeros_like(cell.bias))
+        elif isinstance(cell, nn.Embedding):
+            cell.weight.set_data(initializer(Normal(
+                sigma=std, mean=0.0), cell.weight.shape, cell.weight.dtype))
+            if cell.padding_idx is not None:
+                cell.weight.data[cell.padding_idx] = ops.zeros_like(
+                    cell.weight.data[cell.padding_idx])
 
     def _set_gradient_checkpointing(self, module, value=False):
         if isinstance(module, (MBartDecoder, MBartDecoder)):
