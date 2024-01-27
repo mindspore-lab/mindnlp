1459b1d09dc1d06125bf91fbd3acb08c7efd0e55
lvyufeng <lvyufeng@cqu.edu.cn>
2024-01-27 11:42:44 +0800

    XLMRobertaModel support AutoModel

--- mindnlp/transformers/models/auto/tokenization_auto.py
+++ mindnlp/transformers/models/auto/tokenization_auto.py
@@ -181,13 +181,13 @@ TOKENIZER_MAPPING_NAMES = OrderedDict(
         ("idefics", (None, "LlamaTokenizerFast" if is_tokenizers_available() else None)),
         ("instructblip", ("GPT2Tokenizer", "GPT2TokenizerFast" if is_tokenizers_available() else None)),
         ("jukebox", ("JukeboxTokenizer", None)),
-        (
-            "kosmos-2",
-            (
-                "XLMRobertaTokenizer" if is_sentencepiece_available() else None,
-                "XLMRobertaTokenizerFast" if is_tokenizers_available() else None,
-            ),
-        ),
+        # (
+        #     "kosmos-2",
+        #     (
+        #         "XLMRobertaTokenizer" if is_sentencepiece_available() else None,
+        #         "XLMRobertaTokenizerFast" if is_tokenizers_available() else None,
+        #     ),
+        # ),
         ("layoutlm", ("LayoutLMTokenizer", "LayoutLMTokenizerFast" if is_tokenizers_available() else None)),
         ("layoutlmv2", ("LayoutLMv2Tokenizer", "LayoutLMv2TokenizerFast" if is_tokenizers_available() else None)),
         ("layoutlmv3", ("LayoutLMv3Tokenizer", "LayoutLMv3TokenizerFast" if is_tokenizers_available() else None)),
