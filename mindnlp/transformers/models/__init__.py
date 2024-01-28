870d7c77df48e1c4574189c3fb588b89d970db7e
nate.river <lvyufeng@cqu.edu.cn>
2024-01-28 10:45:25 +0800

    update rwkv (#846)

--- mindnlp/transformers/models/__init__.py
+++ mindnlp/transformers/models/__init__.py
@@ -15,7 +15,7 @@
 """
 Models init
 """
-from . import albert, auto, bart, bert, bloom, clip, codegen, cpm, ernie, ernie_m, chatglm, chatglm2, gpt, gpt_neo, gpt2, \
+from . import albert, auto, bart, bert, bloom, clip, codegen, cpm, ernie, ernie_m, chatglm, chatglm2, gpt, gpt_neo, gpt_neox, gpt2, \
     llama, longformer, luke, mbart, megatron_bert, mistral, mobilebert, nezha, opt, pangu, phi_2, roberta, rwkv, \
     seamless_m4t, seamless_m4t_v2, t5, tinybert, xlm, xlm_roberta, gpt_bigcode, whisper, bark, encodec, \
     graphormer
@@ -34,6 +34,7 @@ from .chatglm import *
 from .chatglm2 import *
 from .gpt import *
 from .gpt_neo import *
+from .gpt_neox import *
 from .gpt2 import *
 from .graphormer import *
 from .llama import *
@@ -74,6 +75,7 @@ __all__.extend(chatglm.__all__)
 __all__.extend(chatglm2.__all__)
 __all__.extend(gpt.__all__)
 __all__.extend(gpt_neo.__all__)
+__all__.extend(gpt_neox.__all__)
 __all__.extend(gpt2.__all__)
 __all__.extend(graphormer.__all__)
 __all__.extend(llama.__all__)
