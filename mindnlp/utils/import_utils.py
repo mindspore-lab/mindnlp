5497938bd8ea901e10146f3bc73080b2e7a707ec
a-strong-python <65645246+a-strong-python@users.noreply.github.com>
2024-01-31 00:09:47 +0800

    add python _LazyModule

--- mindnlp/utils/import_utils.py
+++ mindnlp/utils/import_utils.py
@@ -24,11 +24,13 @@ Import utilities: Utilities related to imports and our lazy inits.
 import os
 import sys
 import warnings
+import importlib
+import importlib.util
 from types import ModuleType
 from collections import OrderedDict
 from functools import wraps
-from typing import Tuple, Union
-import importlib.util
+from typing import Tuple, Union, Any
+from itertools import chain
 
 if sys.version_info >= (3, 8):
     # For Python 3.8 and later
@@ -264,3 +266,64 @@ def direct_transformers_import(path: str, file="__init__.py") -> ModuleType:
     spec.loader.exec_module(module)
     module = sys.modules[name]
     return module
+
+
+class _LazyModule(ModuleType):
+    """
+    _LazyModule 类是一个懒加载模块，用于按需导入模块和对象。
+    """
+
+    # 构造函数，初始化懒加载模块
+    def __init__(self, name, module_file, import_structure, module_spec=None, extra_objects=None):
+        super().__init__(name)
+        self._modules = set(import_structure.keys())
+        self._class_to_module = {}
+        for key, values in import_structure.items():
+            for value in values:
+                self._class_to_module[value] = key
+        # 用于在 IDE 中自动完成
+        self.__all__ = list(import_structure.keys()) + list(chain(*import_structure.values()))
+        self.__file__ = module_file
+        self.__spec__ = module_spec
+        self.__path__ = [os.path.dirname(module_file)]
+        self._objects = {} if extra_objects is None else extra_objects
+        self._name = name
+        self._import_structure = import_structure
+
+    # 用于在 IDE 中自动完成
+    def __dir__(self):
+        result = super().__dir__()
+        # 只添加不在 dir 中的 self.__all__ 元素，因为有些可能已经被访问
+        for attr in self.__all__:
+            if attr not in result:
+                result.append(attr)
+        return result
+
+    # 获取属性时的动态导入
+    def __getattr__(self, name: str) -> Any:
+        if name in self._objects:
+            return self._objects[name]
+        if name in self._modules:
+            value = self._get_module(name)
+        elif name in self._class_to_module.keys():
+            module = self._get_module(self._class_to_module[name])
+            value = getattr(module, name)
+        else:
+            raise AttributeError(f"module {self.__name__} has no attribute {name}")
+
+        setattr(self, name, value)
+        return value
+
+    # 根据模块名执行实际导入
+    def _get_module(self, module_name: str):
+        try:
+            return importlib.import_module("." + module_name, self.__name__)
+        except Exception as e:
+            raise RuntimeError(
+                f"Failed to import {self.__name__}.{module_name} because of the following error (look up to see its"
+                f" traceback):\n{e}"
+            ) from e
+
+    # 用于序列化
+    def __reduce__(self):
+        return (self.__class__, (self._name, self.__file__, self._import_structure))
