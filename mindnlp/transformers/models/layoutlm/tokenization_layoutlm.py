4a5830bad9c0c9e72e92c4c70eb317fbe8d8f84c
FLoutione <3070240252@qq.com>
2024-03-18 20:16:26 +0800

    update pylint_layoutlm

--- mindnlp/transformers/models/layoutlm/tokenization_layoutlm.py
+++ mindnlp/transformers/models/layoutlm/tokenization_layoutlm.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# pylint: disable=C0103
 # pylint: disable=R0205
 # pylint: disable=R0916
 # pylint: disable=R1716
