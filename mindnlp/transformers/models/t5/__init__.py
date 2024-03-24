a712b7290018eec82d515c1db5949843ba554140
nate.river <lvyufeng@cqu.edu.cn>
2024-03-25 04:18:36 +0800

    update bert-crf example (#948)

--- mindnlp/transformers/models/t5/__init__.py
+++ mindnlp/transformers/models/t5/__init__.py
@@ -15,12 +15,10 @@
 """
 T5 Model init
 """
-from . import configuration_t5, modeling_t5, chatyuan_tokenizer, tokenization_t5, tokenization_t5_fast, \
-    tokenization_byt5
+from . import configuration_t5, modeling_t5, chatyuan_tokenizer, tokenization_t5, tokenization_t5_fast
 from .configuration_t5 import *
 from .modeling_t5 import *
 from .tokenization_t5 import *
-from .tokenization_byt5 import *
 from .tokenization_t5_fast import *
 from .chatyuan_tokenizer import *
 
@@ -29,5 +27,4 @@ __all__.extend(modeling_t5.__all__)
 __all__.extend(configuration_t5.__all__)
 __all__.extend(tokenization_t5.__all__)
 __all__.extend(tokenization_t5_fast.__all__)
-__all__.extend(tokenization_byt5.__all__)
 __all__.extend(chatyuan_tokenizer.__all__)
