f8ec3f881d562812a30fc2f9086e004c6a293ef6
WuHuLing <121509135+wjy4399@users.noreply.github.com>
2024-04-22 20:22:18 +0800

    Update model.py

--- mindnlp/peft/tuners/adaption_prompt/model.py
+++ mindnlp/peft/tuners/adaption_prompt/model.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+"""model for adaption prompt tuners."""
 from typing import Dict, List
 
 from mindspore import nn
