c3af91610aebf12847b950d3953c4f2756f84d93
pengyiyang <pengyiyang660@163.com>
2024-05-15 00:52:27 +0800

    pylint

--- mindnlp/transformers/models/cogvlm/modeling_cogvlm.py
+++ mindnlp/transformers/models/cogvlm/modeling_cogvlm.py
@@ -828,8 +828,7 @@ class CogVLMForCausalLM(CogVLMPreTrainedModel):
             transform = transforms.Compose(
                 [
                     vision.Resize(
-                        (image_size, image_size), interpolation= vision.Inter.BICUBIC
-                    ),
+                        (image_size, image_size), interpolation= vision.Inter.BICUBIC),
                     vision.ToTensor(),  
                     vision.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711),is_hwc=False),
                 ]
