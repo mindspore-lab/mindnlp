1c2b330af9a9380f01c177567e3bde2f9a58536b
nate.river <lvyufeng@cqu.edu.cn>
2024-05-21 12:01:07 +0800

    add mindnlp slides (#1089)

--- tests/ut/transformers/pipelines/test_pipelines_document_question_answering.py
+++ tests/ut/transformers/pipelines/test_pipelines_document_question_answering.py
@@ -44,7 +44,7 @@ else:
 # This is a pinned image from a specific revision of a document question answering space, hosted by HuggingFace,
 # so we can expect it to be available.
 INVOICE_URL = (
-    "https://hf-mirror.com/spaces/impira/docquery/resolve/2f6c96314dc84dfda62d40de9da55f2f5165d403/invoice.png"
+    "https://hf.co/spaces/impira/docquery/resolve/2f6c96314dc84dfda62d40de9da55f2f5165d403/invoice.png"
 )
 
 
