5679760df35e1fe746a266e2a2f5378a478c3d0f
Jikai0Wang <risus254@gmail.com>
2024-06-04 20:25:30 +0800

    modify init

--- mindnlp/transformers/models/nystromformer/__init__.py
+++ mindnlp/transformers/models/nystromformer/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from typing import TYPE_CHECKING
-
+from . import configuration_nystromformer, modeling_nystromformer
 from .configuration_nystromformer import *
 from .modeling_nystromformer import *
 
