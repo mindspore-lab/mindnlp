a2a66562bd045a85140a9fb85a8f9648027eb595
yisunlp <yisunlp@outlook.com>
2024-06-07 05:27:53 +0800

    update

--- mindnlp/transformers/models/__init__.py
+++ mindnlp/transformers/models/__init__.py
@@ -93,6 +93,7 @@ from . import (
     longformer,
     luke,
     mamba,
+    marian,
     mbart,
     megatron_bert,
     minicpm,
@@ -228,6 +229,7 @@ from .llava_next import *
 from .longformer import *
 from .luke import *
 from .mamba import *
+from .marian import *
 from .mbart import *
 from .megatron_bert import *
 from .minicpm import *
@@ -363,6 +365,7 @@ __all__.extend(llava_next.__all__)
 __all__.extend(longformer.__all__)
 __all__.extend(luke.__all__)
 __all__.extend(mamba.__all__)
+__all__.extend(marian.__all__)
 __all__.extend(mbart.__all__)
 __all__.extend(megatron_bert.__all__)
 __all__.extend(minicpm.__all__)
