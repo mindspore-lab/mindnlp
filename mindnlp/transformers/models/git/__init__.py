3036f125565460e3b528d16d28fb4510fee6b7dc
Your Name <2163017376@qq.com>
2024-06-07 07:21:54 +0800

    ddd

--- mindnlp/transformers/models/git/__init__.py
+++ mindnlp/transformers/models/git/__init__.py
@@ -3,14 +3,12 @@ This module defines the configuration class for the GIT model.
 """
 
 # ... other code ...
-from . import configuration_git, modeling_git,processing_git,convert_git_to_pytorch
+from . import configuration_git, modeling_git,processing_git
 from .modeling_git import *
 from .configuration_git import *
 from .processing_git import *
-from convert_git_to_pytorch import *
 
 __all__ = []
 __all__.extend(processing_git.__all__)
 __all__.extend(modeling_git.__all__)
 __all__.extend(configuration_git.__all__)
-__all__.extend(convert_git_to_pytorch.__all__)
