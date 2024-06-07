c899c8849859321f9f6f1b36f7975fba37688c26
yisunlp <yisunlp@outlook.com>
2024-06-07 13:00:37 +0800

    update

--- mindnlp/transformers/models/auto/modeling_auto.py
+++ mindnlp/transformers/models/auto/modeling_auto.py
@@ -66,6 +66,7 @@ MODEL_MAPPING_NAMES = OrderedDict(
         ("convnext", "ConvNextModel"),
         ("cvt", "CvtModel"),
         ("deberta", "DebertaModel"),
+        ("decision_transformer", "DecisionTransformerModel"),
         ("efficientformer", "EfficientFormerModel"),
         ("encodec", "EncodecModel"),
         ("esm", "EsmModel"),
