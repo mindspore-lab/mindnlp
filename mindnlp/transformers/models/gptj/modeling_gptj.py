bdee2228d938b6f593cdfc6142dd38eebf07eb38
Jikai0Wang <129487641+Jikai0Wang@users.noreply.github.com>
2024-06-08 18:55:25 +0800

    Update modeling_gptj.py

--- mindnlp/transformers/models/gptj/modeling_gptj.py
+++ mindnlp/transformers/models/gptj/modeling_gptj.py
@@ -48,7 +48,7 @@ _CONFIG_FOR_DOC = "GPTJConfig"
 # Copied from transformers.models.llama.modeling_llama._get_unpad_data
 def _get_unpad_data(attention_mask):
     seqlens_in_batch = attention_mask.sum(axis=-1, dtype=mindspore.int32)
-    indices = ops.nonzero(attention_mask.flatten(), as_tuple=False).flatten()
+    indices = ops.nonzero(attention_mask.flatten()).flatten()
     max_seqlen_in_batch = seqlens_in_batch.max().item()
     cu_seqlens = ops.pad(ops.cumsum(seqlens_in_batch, axis=0, dtype=mindspore.int32), (1, 0))
     return indices, cu_seqlens, max_seqlen_in_batch
