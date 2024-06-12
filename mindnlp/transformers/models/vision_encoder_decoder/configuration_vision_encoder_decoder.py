7c191bc957a0b7f55517f241541f4c440d75eba9
Biaolin Wen <robinbg@foxmail.com>
2024-06-13 00:35:38 +0800

    Update configuration_vision_encoder_decoder.py

--- mindnlp/transformers/models/vision_encoder_decoder/configuration_vision_encoder_decoder.py
+++ mindnlp/transformers/models/vision_encoder_decoder/configuration_vision_encoder_decoder.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+"""VisionEncoderDecoder model configuration"""
 from typing import TYPE_CHECKING
 
 from ...configuration_utils import PretrainedConfig
