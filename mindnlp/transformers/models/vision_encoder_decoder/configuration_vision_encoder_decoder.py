350b6caf2a3b46f0a7b14c587fcc32622c3c2580
Biaolin Wen <robinbg@foxmail.com>
2024-06-13 00:25:43 +0800

    Update configuration_vision_encoder_decoder.py

--- mindnlp/transformers/models/vision_encoder_decoder/configuration_vision_encoder_decoder.py
+++ mindnlp/transformers/models/vision_encoder_decoder/configuration_vision_encoder_decoder.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import TYPE_CHECKING, Any, Mapping, Optional, OrderedDict
+from typing import TYPE_CHECKING
 
 from ...configuration_utils import PretrainedConfig
 from ....utils import logging
