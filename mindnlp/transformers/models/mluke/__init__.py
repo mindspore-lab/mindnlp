c44dd056094024a70bb96dedf0884a0c80a12c20
GJR <gaojiaranzai@163.com>
2024-06-25 17:30:06 +0800

    mkdir mluke

---
+++ mindnlp/transformers/models/mluke/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2024/6/25 17:29
+# @Author  : GJR
+# @FileName: __init__.py.py
+# @Software: PyCharm
+# @Blog    ：https://blog.csdn.net/qq_50882544
+
