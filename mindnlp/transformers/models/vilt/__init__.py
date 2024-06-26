7e734ca556cf2e3b9c631bcb7adb52581698840b
mortyandtimo <g2574385582@gmail.com>
2024-06-26 20:45:26 +0800

    vilt

--- mindnlp/transformers/models/vilt/__init__.py
+++ mindnlp/transformers/models/vilt/__init__.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """VilT model."""
 from . import configuration_vilt, feature_extraction_vilt, image_processing_vilt, modeling_vilt, processing_vilt
 
