f52593c50abf0347ec86a5fc870e2661dbf331a4
Xinyu1225 <129757864+guxinyu1225@users.noreply.github.com>
2024-07-02 11:55:33 +0800

    【社区实习】mBART-50 (#I9UX1G)  (#1344)

--- mindnlp/transformers/models/__init__.py
+++ mindnlp/transformers/models/__init__.py
@@ -117,6 +117,7 @@ from . import (
     marian,
     mask2former,
     mbart,
+    mbart50,
     mctct,
     megatron_bert,
     minicpm,
@@ -301,6 +302,7 @@ from .mamba import *
 from .marian import *
 from .mask2former import *
 from .mbart import *
+from .mbart50 import *
 from .mctct import *
 from .megatron_bert import *
 from .minicpm import *
@@ -484,6 +486,7 @@ __all__.extend(mamba.__all__)
 __all__.extend(marian.__all__)
 __all__.extend(mask2former.__all__)
 __all__.extend(mbart.__all__)
+__all__.extend(mbart50.__all__)
 __all__.extend(mctct.__all__)
 __all__.extend(megatron_bert.__all__)
 __all__.extend(minicpm.__all__)
