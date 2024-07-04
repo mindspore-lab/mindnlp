a1805bd0f1864e82ebb1fb33eb0f3182aef00363
yp8976 <3550405486@qq.com>
2024-07-04 14:30:12 +0800

    add model mgp-str

--- mindnlp/transformers/models/auto/modeling_auto.py
+++ mindnlp/transformers/models/auto/modeling_auto.py
@@ -106,6 +106,7 @@ MODEL_MAPPING_NAMES = OrderedDict(
         ("mask2former", "Mask2FormerModel"),
         ("mbart", "MBartModel"),
         ("mctct","MCTCTModel"),
+        ("mgp-str", "MgpstrForSceneTextRecognition"),
         ("minicpm", "MiniCPMModel"),
         ("mistral", "MistralModel"),
         ("mixtral", "MixtralModel"),
