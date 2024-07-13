26fd5bd4253699b09db33274ff40bf77dac61959
Li Shangjing <1755397894@qq.com>
2024-07-13 10:50:06 +0800

    7/13 10:50

--- mindnlp/transformers/models/deepseek/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import configuration_deepseek, modeling_deepseek, tokenization_deepseek_fast
-from .modeling_deepseek import *
-from .configuration_deepseek import *
-from .tokenization_deepseek_fast import *
-
-__all__ = []
-__all__.extend(modeling_deepseek.__all__)
-__all__.extend(configuration_deepseek.__all__)
-__all__.extend(tokenization_deepseek_fast.__all__)
