4b1ffd18a79e44ab33bce6a65510fc8965ac68c8
Li Shangjing <1755397894@qq.com>
2024-07-14 08:32:23 +0800

    7/14 8:32

--- mindnlp/transformers/models/deepseek_v2/ut.py
+++ mindnlp/transformers/models/deepseek_v2/ut.py
@@ -1,19 +1,6 @@
 import mindspore
 from mindnlp.transformers import AutoTokenizer, AutoModelForCausalLM
 
-# model_name = "deepseek-ai/deepseek-math-7b-base"
-# tokenizer = AutoTokenizer.from_pretrained(model_name)
-# model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=mindspore.bfloat16, device_map="auto")
-# model.generation_config = GenerationConfig.from_pretrained(model_name)
-# model.generation_config.pad_token_id = model.generation_config.eos_token_id
-#
-# text = "The integral of x^2 from 0 to 2 is"
-# inputs = tokenizer(text, return_tensors="pt")
-# outputs = model.generate(**inputs.to(model.device), max_new_tokens=100)
-#
-# result = tokenizer.decode(outputs[0], skip_special_tokens=True)
-# print(result)
-
 tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/DeepSeek-Coder-V2-Lite-Base")
 model = AutoModelForCausalLM.from_pretrained("deepseek-ai/DeepSeek-Coder-V2-Lite-Base", ms_dtype=mindspore.float32)
 input_text = "#write a quick sort algorithm"
