8c64558f2395e60ca7f1284353036cfa4bfa2567
Li Shangjing <1755397894@qq.com>
2024-07-14 23:20:02 +0800

    only deepseek

--- mindnlp/transformers/models/siglip/__init__.py
+++
@@ -1,8 +0,0 @@
-from .modeling_siglip import *
-from .configuration_siglip import *
-from .tokenization_siglip import *
-
-__all__ = []
-__all__.extend(modeling_siglip.__all__)
-__all__.extend(configuration_siglip.__all__)
-__all__.extend(tokenization_siglip.__all__)
