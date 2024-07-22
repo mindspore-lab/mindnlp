7a65f6f99c38c3b87906a9f830e50e610db90209
caly <1679842942@qq.com>
2024-07-22 17:18:28 +0800

    init

--- mindnlp/core/ops/other.py
+++ mindnlp/core/ops/other.py
@@ -133,7 +133,7 @@ def meshgrid(*tensors, indexing=None):
 
 # roll
 
-
+# pylint: disable=unexpected-keyword-arg
 # searchsorted
 def searchsorted(sorted_sequence, values, *, out_int32=False, right=False, side=None, sorter=None):
     if USE_PYBOOST:
