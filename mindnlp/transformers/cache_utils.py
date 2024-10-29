9629fc0a2e8fe4450f54fd5f309487b9ed031588
nate.river <lvyufeng@cqu.edu.cn>
2024-10-29 19:18:36 +0800

    fix llava on Ascend (#1788)

--- mindnlp/transformers/cache_utils.py
+++ mindnlp/transformers/cache_utils.py
@@ -827,7 +827,7 @@ class StaticCache(Cache):
         # Occupied cache == any slot in the 3rd dim (sequence length) holds a non-zero value. To save on compute, let's
         # limit the check to the first batch member and head dimension.
         # TODO: deprecate this function in favor of `cache_position`
-        return (ops.any(self.key_cache[layer_idx][0, 0] != 0, dim=-1)).sum()
+        return (ops.any(self.key_cache[layer_idx][0, 0] != 0, dim=-1)).sum().item()
 
     def get_max_length(self) -> Optional[int]:
         """Returns the maximum sequence length of the cached states."""
@@ -1012,7 +1012,7 @@ class EncoderDecoderCache(Cache):
         if len(self.self_attention_cache.key_cache) > 1 and self.self_attention_cache.key_cache[layer_idx] == []:
             return 0
 
-        return (ops.any(self.self_attention_cache.key_cache[layer_idx][0, 0] != 0, dim=-1)).sum()
+        return (ops.any(self.self_attention_cache.key_cache[layer_idx][0, 0] != 0, dim=-1)).sum().item()
 
     def reset(self):
         if hasattr(self.self_attention_cache, "reset"):
