f7a11416a4c91edae8a2f75b655192f577413d2b
13674301140 <tridu33@qq.com>
2024-12-17 01:18:38 +0800

    fix: avoid parallel bug for dropout operator replacing GRAPH_MODE by default PYNATIVE_MODe

--- mindnlp/accelerate/utils/constants.py
+++ mindnlp/accelerate/utils/constants.py
@@ -5,14 +5,16 @@ import numpy
 from .dataclasses import DistributedType
 
 
+_random_seed = numpy.random.randint(1000)
+
+
 def _prepare_data_parallel_native_minspore():
     # initialize data parallel hcc backend for data_loader and Trainer API
-    mindspore.set_context(mode=mindspore.GRAPH_MODE)
     mindspore.set_auto_parallel_context(parallel_mode=mindspore.ParallelMode.DATA_PARALLEL, gradients_mean=True)
     mindspore.communication.init()
-    random_seed = numpy.random.randint(10000)
-    mindspore.set_seed(random_seed)
-    
+    mindspore.set_seed(_random_seed)
+
+
 def detect_accelerate_distributed_type():
     """
     detect distributed_type
@@ -21,13 +23,12 @@ def detect_accelerate_distributed_type():
         _type_: According to the factors such as the available parallel software and hardware environment of the current system and the user-specified parallel scheme,
           the optimal parallel strategy is comprehensively decided in different situations.
     """
-    if os.environ.get("MULTI_NPU", None) == "true": 
+    if os.environ.get("MULTI_NPU", None) == "true":
         _prepare_data_parallel_native_minspore()
         return DistributedType.MULTI_NPU
-    if os.environ.get("ACCELERATE_USE_MINDFORMERS", "false") == "true": 
+    if os.environ.get("ACCELERATE_USE_MINDFORMERS", "false") == "true":
         return DistributedType.MINDFORMERS
     else:
         return DistributedType.NO
 
 accelerate_distributed_type = detect_accelerate_distributed_type()
-    
