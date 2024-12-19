fix sbert precision problem on mindnlp.sentence (#1873)

mindnlp/sentence/models/transformer.py
@@ -1,45 +1,79 @@
-# Copyright 2023 Huawei Technologies Co., Ltd
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ============================================================================
-"""Sentence transformer module"""
+"""transformer model"""
+from __future__ import annotations
 
 import json
+import logging
 import os
-from typing import Dict, List, Optional, Tuple, Union
+from pathlib import Path
+from typing import Any, Callable
 
-from mindnlp.core import nn
-from mindnlp.transformers import AutoModel, AutoTokenizer
+import mindspore
+from ...core import nn, ops
+from ...transformers import AutoConfig, AutoModel, AutoTokenizer, MT5Config, T5Config
+from ...utils.peft_utils import find_adapter_config_file
+from ...peft import PeftConfig, PeftModel, PeftModelForFeatureExtraction
+
+logger = logging.getLogger(__name__)
+
+
+def _save_pretrained_wrapper(_save_pretrained_fn: Callable, subfolder: str) -> Callable[..., None]:
+    def wrapper(save_directory: str | Path, **kwargs) -> None:
+        os.makedirs(Path(save_directory) / subfolder, exist_ok=True)
+        return _save_pretrained_fn(Path(save_directory) / subfolder, **kwargs)
+
+    return wrapper
 
 
 class Transformer(nn.Module):
+    """Hugging Face AutoModel to generate token embeddings.
+    Loads the correct class, e.g. BERT / RoBERTa etc.
+
+    Args:
+        model_name_or_path: Hugging Face models name
+            (https://huggingface.co/models)
+        max_seq_length: Truncate any inputs longer than max_seq_length
+        model_args: Keyword arguments passed to the Hugging Face
+            Transformers model
+        tokenizer_args: Keyword arguments passed to the Hugging Face
+            Transformers tokenizer
+        config_args: Keyword arguments passed to the Hugging Face
+            Transformers config
+        cache_dir: Cache dir for Hugging Face Transformers to store/load
+            models
+        do_lower_case: If true, lowercases the input (independent if the
+            model is cased or not)
+        tokenizer_name_or_path: Name or path of the tokenizer. When
+            None, then model_name_or_path is used
+    """
+
+    save_in_root: bool = True
+
     def __init__(
         self,
         model_name_or_path: str,
-        max_seq_length: Optional[int] = None,
-        model_args: Dict = {},
-        cache_dir: Optional[str] = None,
-        tokenizer_args: Dict = {},
+        max_seq_length: int | None = None,
+        model_args: dict[str, Any] | None = None,
+        tokenizer_args: dict[str, Any] | None = None,
+        config_args: dict[str, Any] | None = None,
+        cache_dir: str | None = None,
         do_lower_case: bool = False,
         tokenizer_name_or_path: str = None,
-    ):
-        super(Transformer, self).__init__()
+    ) -> None:
+        super().__init__()
         self.config_keys = ["max_seq_length", "do_lower_case"]
         self.do_lower_case = do_lower_case
+        if model_args is None:
+            model_args = {}
+        if tokenizer_args is None:
+            tokenizer_args = {}
+        if config_args is None:
+            config_args = {}
 
-        # config = AutoConfig.from_pretrained(model_name_or_path, **model_args, cache_dir=cache_dir)
-        self._load_model(model_name_or_path, cache_dir, **model_args)
+        config = self._load_config(model_name_or_path, cache_dir, config_args)
+        self._load_model(model_name_or_path, config, cache_dir, **model_args)
 
+        if max_seq_length is not None and "model_max_length" not in tokenizer_args:
+            tokenizer_args["model_max_length"] = max_seq_length
         self.tokenizer = AutoTokenizer.from_pretrained(
             tokenizer_name_or_path if tokenizer_name_or_path is not None else model_name_or_path,
             cache_dir=cache_dir,
@@ -60,47 +94,104 @@ class Transformer(nn.Module):
         if tokenizer_name_or_path is not None:
             self.auto_model.config.tokenizer_class = self.tokenizer.__class__.__name__
 
-    def _load_model(self, model_name_or_path, cache_dir, **model_args):
+    def _load_config(self, model_name_or_path: str, cache_dir: str | None, config_args: dict[str, Any]):
+        """Loads the configuration of a model"""
+        if (
+            find_adapter_config_file(
+                model_name_or_path,
+                token=config_args.get("token"),
+                revision=config_args.get("revision"),
+                local_files_only=config_args.get("local_files_only", False),
+            )
+            is not None
+        ):
+
+            return PeftConfig.from_pretrained(model_name_or_path, **config_args, cache_dir=cache_dir)
+
+        return AutoConfig.from_pretrained(model_name_or_path, **config_args, cache_dir=cache_dir)
+
+    def _load_model(self, model_name_or_path, config, cache_dir, **model_args) -> None:
         """Loads the transformer model"""
-        # config = AutoConfig.from_pretrained(model_name_or_path)
-        config = None
-        self.auto_model = AutoModel.from_pretrained(
+        if isinstance(config, T5Config):
+            self._load_t5_model(model_name_or_path, config, cache_dir, **model_args)
+        elif isinstance(config, MT5Config):
+            self._load_mt5_model(model_name_or_path, config, cache_dir, **model_args)
+        else:
+            self.auto_model = AutoModel.from_pretrained(
+                model_name_or_path, config=config, cache_dir=cache_dir, **model_args
+            )
+        self._load_peft_model(model_name_or_path, config, cache_dir, **model_args)
+
+    def _load_peft_model(self, model_name_or_path, config, cache_dir, **model_args) -> None:
+        if isinstance(config, PeftConfig):
+            self.auto_model = PeftModel.from_pretrained(
+                self.auto_model, model_name_or_path, config=config, cache_dir=cache_dir, **model_args
+            )
+
+
+    def _load_t5_model(self, model_name_or_path, config, cache_dir, **model_args) -> None:
+        """Loads the encoder model from T5"""
+        from mindnlp.transformers import T5EncoderModel
+
+        T5EncoderModel._keys_to_ignore_on_load_unexpected = ["decoder.*"]
+        self.auto_model = T5EncoderModel.from_pretrained(
             model_name_or_path, config=config, cache_dir=cache_dir, **model_args
         )
 
-    def __repr__(self):
-        return "Transformer({}) with Transformer model: {} ".format(
-            self.get_config_dict(), self.auto_model.__class__.__name__
+    def _load_mt5_model(self, model_name_or_path, config, cache_dir, **model_args) -> None:
+        """Loads the encoder model from T5"""
+        from mindnlp.transformers import MT5EncoderModel
+
+        MT5EncoderModel._keys_to_ignore_on_load_unexpected = ["decoder.*"]
+        self.auto_model = MT5EncoderModel.from_pretrained(
+            model_name_or_path, config=config, cache_dir=cache_dir, **model_args
         )
 
-    def forward(self, features):
+    def __repr__(self) -> str:
+        return f"Transformer({self.get_config_dict()}) with Transformer model: {self.auto_model.__class__.__name__} "
+
+    def forward(self, features: dict[str, mindspore.Tensor], **kwargs) -> dict[str, mindspore.Tensor]:
         """Returns token_embeddings, cls_token"""
         trans_features = {"input_ids": features["input_ids"], "attention_mask": features["attention_mask"]}
         if "token_type_ids" in features:
             trans_features["token_type_ids"] = features["token_type_ids"]
 
-        output_states = self.auto_model(**trans_features, return_dict=False)
+        output_states = self.auto_model(**trans_features, **kwargs, return_dict=False)
         output_tokens = output_states[0]
 
-        features.update({"token_embeddings": output_tokens, "attention_mask": features["attention_mask"]})
+        # If the AutoModel is wrapped with a PeftModelForFeatureExtraction, then it may have added virtual tokens
+        # We need to extend the attention mask to include these virtual tokens, or the pooling will fail
 
-        if self.auto_model.config.output_hidden_states:
-            all_layer_idx = 2
+        if (
+            isinstance(self.auto_model, PeftModelForFeatureExtraction)
+            and self.auto_model.active_peft_config.is_prompt_learning
+        ):
+            batch_size = output_tokens.size(0)
+            attention_mask = features["attention_mask"]
+            prefix_attention_mask = ops.ones(
+                batch_size, self.auto_model.active_peft_config.num_virtual_tokens
+            )
+            features["attention_mask"] = ops.cat((prefix_attention_mask, attention_mask), dim=1)
+
+        features["token_embeddings"] = output_tokens
+
+        if self.auto_model.config.output_hidden_states and len(output_states) > 2:
+            all_layer_idx = 2  # I.e. after last_hidden_states and pooler_output
             if len(output_states) < 3:  # Some models only output last_hidden_states and all_hidden_states
                 all_layer_idx = 1
 
             hidden_states = output_states[all_layer_idx]
-            features.update({"all_layer_embeddings": hidden_states})
+            features["all_layer_embeddings"] = hidden_states
 
         return features
 
     def get_word_embedding_dimension(self) -> int:
         return self.auto_model.config.hidden_size
 
-    def tokenize(self, texts: Union[List[str], List[Dict], List[Tuple[str, str]]], padding: Union[str, bool] = True):
-        """
-        Tokenizes a text and maps tokens to token-ids
-        """
+    def tokenize(
+        self, texts: list[str] | list[dict] | list[tuple[str, str]], padding: str | bool = True
+    ) -> dict[str, mindspore.Tensor]:
+        """Tokenizes a text and maps tokens to token-ids"""
         output = {}
         if isinstance(texts[0], str):
             to_tokenize = [texts]
@@ -137,18 +228,18 @@ class Transformer(nn.Module):
         )
         return output
 
-    def get_config_dict(self):
+    def get_config_dict(self) -> dict[str, Any]:
         return {key: self.__dict__[key] for key in self.config_keys}
 
-    def save(self, output_path: str, safe_serialization: bool = True):
+    def save(self, output_path: str, safe_serialization: bool = True) -> None:
         self.auto_model.save_pretrained(output_path, safe_serialization=safe_serialization)
         self.tokenizer.save_pretrained(output_path)
 
         with open(os.path.join(output_path, "sentence_bert_config.json"), "w") as fOut:
             json.dump(self.get_config_dict(), fOut, indent=2)
 
-    @staticmethod
-    def load(input_path: str):
+    @classmethod
+    def load(cls, input_path: str) -> Transformer:
         # Old classes used other config names than 'sentence_bert_config.json'
         for config_name in [
             "sentence_bert_config.json",
@@ -166,6 +257,10 @@ class Transformer(nn.Module):
         with open(sbert_config_path) as fIn:
             config = json.load(fIn)
         # Don't allow configs to set trust_remote_code
-        if "model_args" in config:
+        if "model_args" in config and "trust_remote_code" in config["model_args"]:
             config["model_args"].pop("trust_remote_code")
-        return Transformer(model_name_or_path=input_path, **config)
+        if "tokenizer_args" in config and "trust_remote_code" in config["tokenizer_args"]:
+            config["tokenizer_args"].pop("trust_remote_code")
+        if "config_args" in config and "trust_remote_code" in config["config_args"]:
+            config["config_args"].pop("trust_remote_code")
+        return cls(model_name_or_path=input_path, **config)