6582b71e333ba1d5d0faeece6285765edcacb663
nate.river <lvyufeng@cqu.edu.cn>
2025-09-28 19:32:03 +0800

    fix ms2.7 on GPU (#2182)

--- mindtorch/executor.py
+++ mindtorch/executor.py
@@ -1,8 +1,12 @@
 import mindtorch
 from .dispatcher import dispatcher
+from .configs import MS27
 
 def execute(func_name, *args, **kwargs):
     out, device = dispatcher.dispatch(func_name, *args, **kwargs)
+    # if MS27:
+    #     return out
+
     if not isinstance(out, (tuple, list)):
         out._device = device
     else:
