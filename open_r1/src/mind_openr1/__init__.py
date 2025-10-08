9b389bbe98a7ec1708249d6265b20b662f72c57a
guojialiang <2802427218@qq.com>
2025-10-08 13:26:26 +0800

    add: sft

--- open_r1/src/mind_openr1/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Mind-OpenR1: MindSpore implementation of OpenR1
-"""
-
-from .sft_trainer import SFTTrainer, SFTConfig
-from .configs import ScriptArguments
-
-__all__ = ["SFTTrainer", "SFTConfig", "ScriptArguments"]
