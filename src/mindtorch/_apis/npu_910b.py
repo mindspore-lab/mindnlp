6472efe3dd5f47237209166af482b6ee1abb3741
allendyu <129837534+allendyu@users.noreply.github.com>
2025-12-16 18:18:35 +0800

    modify inplace_sub (#2334)

--- src/mindtorch/_apis/npu_910b.py
+++ src/mindtorch/_apis/npu_910b.py
@@ -2506,6 +2506,8 @@ def raw_adam(param, exp_avg, exp_avg_sq, beta1_power, beta2_power, lr, beta1, be
     return legacy.adam(param, exp_avg, exp_avg_sq, beta1_power, beta2_power, lr, beta1, beta2, epsilon, grad, False, False)
 
 def inplace_sub(input, other):
+    if type(other) == int:
+        other = mindspore.Tensor(other)
     return pyboost.inplace_sub_ext_op(input, other)
 
 def isfinite(input):
