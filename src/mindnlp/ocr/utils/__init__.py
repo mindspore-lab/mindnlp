dfe512dbe0d20baf09f9fad8ba70cc8814bc0e7c
3023209092-姚子辰 <731107711@qq.com>
2026-01-24 13:11:13 +0800

    feat(ocr): Export cache_manager components in utils __init__.py

--- src/mindnlp/ocr/utils/__init__.py
+++ src/mindnlp/ocr/utils/__init__.py
@@ -5,11 +5,16 @@
 from .logger import get_logger
 from .image_utils import download_image_from_url, validate_image_format
 from .text_utils import clean_text, extract_json_from_text
+from .cache_manager import CacheConfig, KVCacheManager, detect_flash_attention_support, get_optimal_cache_config
 
 __all__ = [
     'get_logger',
     'download_image_from_url',
     'validate_image_format',
     'clean_text',
-    'extract_json_from_text'
+    'extract_json_from_text',
+    'CacheConfig',
+    'KVCacheManager',
+    'detect_flash_attention_support',
+    'get_optimal_cache_config'
 ]
