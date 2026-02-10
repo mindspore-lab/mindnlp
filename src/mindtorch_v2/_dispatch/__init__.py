511f036398f83eff8da93596d5c0881b993606cd
lvyufeng <lvyufeng@cqu.edu.cn>
2026-02-10 21:42:55 +0800

    feat(mindtorch_v2): reset package skeleton

--- src/mindtorch_v2/_dispatch/__init__.py
+++
@@ -1,5 +0,0 @@
-from .keys import DispatchKey
-from .registry import register_op, get_op_impl
-from .dispatcher import dispatch
-
-__all__ = ['DispatchKey', 'register_op', 'get_op_impl', 'dispatch']
