24bbf8f78415318838263793c86f6af380d43e00
lvyufeng <lvyufeng@cqu.edu.cn>
2026-02-10 21:47:45 +0800

    chore(mindtorch_v2): remove legacy v2 code

--- src/mindtorch_v2/_ops/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Operations module."""
-from .base import Op
-from .registry import get_op, execute_op, register_op
--- tests/mindtorch_v2/test_scaffold.py
+++
@@ -1,3 +0,0 @@
-def test_import():
-    import mindtorch_v2 as torch
-    assert hasattr(torch, '__version__')
