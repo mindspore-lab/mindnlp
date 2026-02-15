8806217798b4a7a7b1243899a3708b9699ae582f
lvyufeng <lvyufeng@cqu.edu.cn>
2026-02-15 13:32:43 +0800

    Add saved tensor hooks API

--- src/mindtorch_v2/_autograd/node.py
+++ src/mindtorch_v2/_autograd/node.py
@@ -1,14 +1,27 @@
+from .graph import current_saved_tensors_hooks
+
+
 class SavedTensor:
     def __init__(self, tensor):
-        self.tensor = tensor
+        self._tensor_ref = tensor
         self._saved_version = tensor._version_counter.value
+        hooks = current_saved_tensors_hooks()
+        self._hooks = hooks
+        if hooks is None:
+            self._packed = None
+        else:
+            pack, _ = hooks
+            self._packed = pack(tensor)
 
     def materialize(self):
-        if self.tensor._version_counter.value != self._saved_version:
+        if self._tensor_ref._version_counter.value != self._saved_version:
             raise RuntimeError(
                 "one of the variables needed for gradient computation has been modified by an inplace operation"
             )
-        return self.tensor
+        if self._hooks is None:
+            return self._tensor_ref
+        _, unpack = self._hooks
+        return unpack(self._packed)
 
 
 class Node:
