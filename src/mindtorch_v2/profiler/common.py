27b696845280a0b2097900dde297abf3a5493bdb
lvyufeng <lvyufeng@cqu.edu.cn>
2026-02-28 12:15:55 +0800

    feat(mindtorch_v2): add profiler schedule and action api

--- src/mindtorch_v2/profiler/common.py
+++ src/mindtorch_v2/profiler/common.py
@@ -8,3 +8,12 @@ class ProfilerActivity(Enum):
     NPU = "NPU"
     CUDA = "NPU"
     GPU = "NPU"
+
+
+class ProfilerAction(Enum):
+    """Profiler schedule action enum."""
+
+    NONE = 0
+    WARMUP = 1
+    RECORD = 2
+    RECORD_AND_SAVE = 3
