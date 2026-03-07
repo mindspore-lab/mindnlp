9a263faa454f2167abb45294fe9cc0c7f7cc8460
lvyufeng <lvyufeng@cqu.edu.cn>
2026-03-07 22:24:23 +0800

    feat(mindtorch_v2): add distributed checkpoint save/load mvp APIs

--- src/mindtorch_v2/distributed/checkpoint/__init__.py
+++ src/mindtorch_v2/distributed/checkpoint/__init__.py
@@ -1,8 +1,10 @@
 """Distributed checkpoint APIs available in mindtorch_v2."""
 
-from .state_dict import get_state_dict, set_state_dict
+from .state_dict import get_state_dict, load, save, set_state_dict
 
 __all__ = [
     "get_state_dict",
+    "save",
+    "load",
     "set_state_dict",
 ]
